59a5846cadacc2f4dad6ff3d5eda9a47af253340
Gaurang <gpambasana@gmail.com>
2021-06-19 13:01:47 +0530

    added code comments

--- odd-even.js
+++ odd-even.js
@@ -1,7 +1,16 @@
+// Any Number which is divisible by 2 is "EVEN" Number
+// Any Number which is not divisible by 2 is "ODD" Number
+
+// % (Modulo) operator return reminder
+// Directly Number % 2 == 0 -> EVEN
+// Directly Number % 2 != 0 -> ODD
+
+// function check if number is odd
 const isOdd = (number) => {
   return Number(number) % 2 !== 0;
 };
 
+// function to check if number is even
 const isEven = (number) => {
   return Number(number) % 2 === 0;
 };
