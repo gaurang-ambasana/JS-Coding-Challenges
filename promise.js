f470685b013f6385e46b5077eb35ecbc89159d61
Gaurang <gpambasana@gmail.com>
2021-06-19 12:42:33 +0530

    added code comments

--- promise.js
+++ promise.js
@@ -1,8 +1,9 @@
 const fetchData = async () => {
-  const { data } = await fetch("https://www.zapya.com/");
-  return data;
+  //use any API/URL
+  const { data } = await fetch("https://www.zapya.com/"); // awaits for network call to return something
+  return data; // returns destructured data
 };
 
-fetchData()
-  .then((res) => console.log(res))
-  .catch((err) => console.error(err));
+fetchData() // can't assign directly to variable as it's async
+  .then((res) => console.log(res)) // once data is retrived logs it
+  .catch((err) => console.error(err)); // handling error if any
