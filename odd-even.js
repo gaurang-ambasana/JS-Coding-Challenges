299cc1b690e7567831da25aef97cf71d1270db85
Gaurang <gpambasana@gmail.com>
2021-06-19 13:02:32 +0530

    added more comments

--- odd-even.js
+++ odd-even.js
@@ -15,5 +15,5 @@ const isEven = (number) => {
   return Number(number) % 2 === 0;
 };
 
-console.log(isOdd(7));
-console.log(isEven(7));
+console.log(isOdd(7)); // -> True
+console.log(isEven(7)); // -> False
