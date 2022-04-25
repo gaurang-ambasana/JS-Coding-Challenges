5ecfcd19a47ab09cd36276885efa10a463142a1a
Gaurang Ambasana <gpambasana@gmail.com>
2022-04-25 20:18:05 +0530

    Time: 112 ms (10.76%), Space: 42.6 MB (17.13%) - LeetHub

---
+++ 2042-check-if-numbers-are-ascending-in-a-sentence/2042-check-if-numbers-are-ascending-in-a-sentence.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var areNumbersAscending = function(s) {
+    const nums = s.split(' ').map(Number).filter(e => !isNaN(e));
+    return [...nums].sort((a, b) => a - b).every((ele, idx) => ele === nums[idx]) && [...new Set(nums)].length === nums.length;
+};
