c8ae4e9fbfb86b651d7f837b207668357c73f8a8
Gaurang Ambasana <gpambasana@gmail.com>
2022-04-25 19:30:33 +0530

    Time: 72 ms (75.14%), Space: 42.3 MB (77.39%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+    let result = '';
+    const strLengths = strs.map(s => s.length);
+    const minLen = Math.min(...strLengths);
+    const shortestStr = strs[strLengths.indexOf(minLen)];
+    
+    for (let i = 1; i <= minLen; i++) {
+        const prefix = shortestStr.substring(0,i);
+        
+        if(strs.every(s => s.startsWith(prefix))) {
+            if(result.length < prefix.length) {
+                result = prefix;
+            }
+        }
+    }
+    
+    return result;
+};
