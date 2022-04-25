5f1cbdd67bcc6dba8270c9b79195e9220be364a6
Gaurang Ambasana <gpambasana@gmail.com>
2022-04-25 19:53:22 +0530

    Time: 85 ms (46.65%), Space: 48.1 MB (6.62%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+  let len = Infinity;
+    
+    while(len !== s.length) {
+        len = s.length;
+        s = s.replace('()','');
+        s = s.replace('{}','');
+        s = s.replace('[]','');
+    }
+    
+    return s.length === 0;
+};
