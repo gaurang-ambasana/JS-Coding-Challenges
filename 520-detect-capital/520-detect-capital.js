0559803c3bdef6833c3042b5a5da76866c9bc063
Gaurang Ambasana <gpambasana@gmail.com>
2022-04-25 20:03:58 +0530

    Time: 72 ms (77.63%), Space: 42.4 MB (47.89%) - LeetHub

---
+++ 520-detect-capital/520-detect-capital.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var detectCapitalUse = function(word) {
+    const prefix = word.substring(0, 1);
+    const rest = word.substring(1, word.length);
+    
+    return word === word.toUpperCase() || word === word.toLowerCase() || (prefix === prefix.toUpperCase() && rest === rest.toLowerCase());
+};
