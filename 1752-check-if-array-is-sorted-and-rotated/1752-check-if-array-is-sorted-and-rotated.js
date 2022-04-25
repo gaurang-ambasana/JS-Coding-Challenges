f040c5156cee03a6ce3a137a97aba32b725dd617
Gaurang Ambasana <gpambasana@gmail.com>
2022-04-25 20:12:53 +0530

    Time: 95 ms (25.00%), Space: 43.8 MB (18.33%) - LeetHub

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var check = function(nums) {
+    const rotateArr = (...arr) => {
+        arr.unshift(arr.pop());
+        return arr;
+    }
+    
+    const sortedNums = [...nums].sort((a, b) => a - b);
+    let rotatedArr = [...sortedNums];
+    
+    for(let i = 0; i < nums.length; i++) {
+        rotatedArr = rotateArr(...rotatedArr);
+        if(rotatedArr.every((ele, idx) => ele === nums[idx])) return true;
+    }
+    
+    return false;
+};
+
