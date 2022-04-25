da315e5bc21b3a4a7faee0a5757ffdf361f5ede1
Gaurang Ambasana <gpambasana@gmail.com>
2022-04-25 20:19:00 +0530

    Time: 74 ms (64.21%), Space: 41.7 MB (90.30%) - LeetHub

---
+++ 2124-check-if-all-as-appears-before-all-bs/2124-check-if-all-as-appears-before-all-bs.js
@@ -0,0 +1,7 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var checkString = function(s) {
+    return s.includes('a') && s.includes('b') ? s.lastIndexOf('a') < s.indexOf('b') : s.includes('a') || s.includes('b');
+};
