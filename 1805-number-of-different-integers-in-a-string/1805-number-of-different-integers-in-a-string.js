394c0bb03c26001add4c75b8e2bb455e6f703dca
Gaurang Ambasana <gpambasana@gmail.com>
2022-04-30 12:34:17 +0530

    Time: 78 ms (58.70%), Space: 43.6 MB (42.39%) - LeetHub

--- 1805-number-of-different-integers-in-a-string/1805-number-of-different-integers-in-a-string.js
+++ 1805-number-of-different-integers-in-a-string/1805-number-of-different-integers-in-a-string.js
@@ -9,7 +9,7 @@ var numDifferentIntegers = function(word) {
     for (let i = 0; i <= word.length; i++) {
         if(isNaN(parseInt(word[i]))) {
             if(num !== '0') {
-                (nums.includes(parseInt(num)) && num != Infinity) || nums.push(parseInt(num));
+                nums.includes(BigInt(num)) || nums.push(BigInt(num));
                 num = '0'; 
             }
         } else num += word[i].toString();
