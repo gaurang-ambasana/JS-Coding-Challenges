e2d16bd19cf3da951132ae15554b420cb5dba6f6
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-01 20:00:28 +0530

    Time: 121 ms (10.19%), Space: 44.5 MB (6.25%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.js
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.js
@@ -4,6 +4,7 @@
  */
 var finalValueAfterOperations = function(operations) {
     let X = 0;
-    for (let i = 0; i < operations.length; i++) eval(operations[i]);
+    for (let i = 0; i < operations.length; i++) 
+        eval(operations[i]);
     return X;
 };
