00e3beef085fc1f17d5eb2d50463caad31ff8979
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-03 01:03:10 +0530

    Time: 112 ms (26.85%), Space: 44.3 MB (67.59%) - LeetHub

--- 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.js
+++ 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.js
@@ -8,15 +8,11 @@ const checkAlmostEquivalent = (word1, word2) => {
     const freqOfWord1 = getFreqOfWord(word1);
     const freqOfWord2 = getFreqOfWord(word2);
     
-    return Object.entries(freqOfWord1).every(([key, value]) => {
-        if (key in freqOfWord2) {
-            const diff = Math.abs(freqOfWord2[key] - value);
-            return diff < 4;
-        } else return value < 4;
-    }) && Object.entries(freqOfWord2).every(([key, value]) => {
-        if (key in freqOfWord1) {
-            const diff = Math.abs(freqOfWord1[key] - value);
-            return diff < 4;
-        } else return value < 4;
-    });
+    return Object.entries(freqOfWord1).every(([key, value]) => 
+        key in freqOfWord2 ? 
+            Math.abs(freqOfWord2[key] - value) < 4 : value < 4
+    ) && Object.entries(freqOfWord2).every(([key, value]) => 
+        key in freqOfWord1 ?
+            Math.abs(freqOfWord1[key] - value) < 4 : value < 4
+    );
 };
