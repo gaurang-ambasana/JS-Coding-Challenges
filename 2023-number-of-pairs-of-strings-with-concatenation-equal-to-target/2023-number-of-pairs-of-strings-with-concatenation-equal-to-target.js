13ccbfea89e92edbd37a46031a0cd4a933ba941d
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-02 23:36:48 +0530

    Time: 97 ms (55.95%), Space: 48.1 MB (63.09%) - LeetHub

---
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string[]} nums
+ * @param {string} target
+ * @return {number}
+ */
+var numOfPairs = function(nums, target) {
+    let count = 0;
+    let start = 0;
+    
+    while (start < nums.length) {
+        for (let i = 0; i < nums.length; i++) {
+            if(i !== start) {
+                (nums[i] + nums[start]) === target && ++count;
+            }
+        }
+        ++start;
+    }
+    
+    return count;
+};
