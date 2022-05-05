89e782820e65cd7d488fa06728ef98229dc3a10f
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-05 19:05:06 +0530

    Time: 120 ms (32.10%), Space: 44.9 MB (62.96%) - LeetHub

---
+++ 2032-two-out-of-three/2032-two-out-of-three.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number[]} nums3
+ * @return {number[]}
+ */
+var twoOutOfThree = function(nums1, nums2, nums3) {
+    return [...new Set([...nums1, ...nums2, ...nums3].filter(val => 
+        (nums1.includes(val) && nums2.includes(val)) || 
+        (nums3.includes(val) && nums2.includes(val)) || 
+        (nums1.includes(val) && nums3.includes(val))))];
+};
