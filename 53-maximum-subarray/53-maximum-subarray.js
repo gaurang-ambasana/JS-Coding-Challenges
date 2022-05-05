b1b942a19c33e57f95e79b4712e5d6be34dd95a8
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-05 18:01:50 +0530

    Time: 138 ms (17.63%), Space: 50.3 MB (73.33%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -6,8 +6,8 @@ var maxSubArray = function(nums) {
     let max = nums[0];
     let prev = -Infinity;
     
-    for (const current of nums) {
-        prev = Math.max(current, current + prev);
+    for (let i = 0; i < nums.length; i++) {
+        prev = Math.max(nums[i], nums[i] + prev);
         max = Math.max(prev, max);
     }
     
