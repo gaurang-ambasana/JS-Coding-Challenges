fd992b4a3a30198633840e8679177e4b4d14eb4d
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-05 17:48:53 +0530

    Time: 86 ms (88.77%), Space: 50.2 MB (87.30%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,5 +3,5 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    return nums.some((ele, i) => i !== nums.lastIndexOf(ele));
+    return new Set(nums).size !== nums.length;
 };
