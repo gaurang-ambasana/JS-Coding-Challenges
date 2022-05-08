112e3ca5c9b47b0a6a8cf4aa63725beb6519229e
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-09 00:44:24 +0530

    Time: 97 ms (36.61%), Space: 44 MB (45.40%) - LeetHub

---
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersect = function(nums1, nums2) {
+    if (nums1 > nums2) 
+        return nums1.filter(n => nums1.includes(n) && nums2.includes(n) && nums2.splice(nums2.indexOf(n), 1));
+    else return nums2.filter(n => nums1.includes(n) && nums2.includes(n) && nums1.splice(nums1.indexOf(n), 1));
+};
