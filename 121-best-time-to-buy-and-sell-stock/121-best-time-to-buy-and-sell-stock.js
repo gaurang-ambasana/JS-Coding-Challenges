e5b476ca76629eaa3d425ab8b4a7ed09106996d6
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-09 01:30:09 +0530

    Time: 97 ms (68.12%), Space: 51.8 MB (48.38%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -5,8 +5,9 @@
 var maxProfit = function(prices) {
     let profit = 0, left = prices[0];
 
-    for (let i = 1; i < prices.length ; i++) {
+    for (let i = 1; i < prices.length; i++) {
         const right = prices[i];
+        if (left === right) continue;
         if (left > right) left = right;
         const currentProfit = right - left;
         if (currentProfit > profit) profit = currentProfit;
