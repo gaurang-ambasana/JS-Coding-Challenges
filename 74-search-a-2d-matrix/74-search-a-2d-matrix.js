1b7cc35d1879f12bfd3f3a4858c53fe5ccdd069e
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-10 00:34:08 +0530

    Time: 124 ms (5.21%), Space: 42 MB (76.16%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.js
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.js
@@ -4,5 +4,5 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    return Boolean(matrix.find(row => row.includes(target)));
+    return matrix.findIndex(row => row.includes(target)) > -1;
 };
