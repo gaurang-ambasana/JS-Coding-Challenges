d965c069a398b0e621f8d91edd40c2c83bd3be47
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-10 01:10:07 +0530

    Time: 130 ms (74.17%), Space: 50.4 MB (74.54%) - LeetHub

---
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[][]} matrix
+ * @return {boolean}
+ */
+var checkValid = function(matrix) {
+    let rows = new Set();
+    let cols = new Set();
+    
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix.length; j++) {
+            rows.add(matrix[i][j]);
+            cols.add(matrix[j][i]);
+        }
+        if (matrix.length !== rows.size || matrix.length !== cols.size) 
+            return false;
+        
+        rows.clear();
+        cols.clear();
+    }
+    return true;
+};
