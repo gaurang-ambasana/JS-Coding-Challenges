c2068c5611c2d271de11b4fc587dffcf5be719bf
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-10 00:28:16 +0530

    Time: 102 ms (57.60%), Space: 47 MB (29.15%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.js
+++ 36-valid-sudoku/36-valid-sudoku.js
@@ -3,25 +3,29 @@
  * @return {boolean}
  */
 var isValidSudoku = function (board) {
-  const rowsValid = board.every(isValidRow);
-  const colsValid = board
-    .reduce((acc, row) => {
+  for (let i = 0; i < board.length; i++)
+      if (!isValidRow(board[i])) return false;
+    
+  const columns = board.reduce((acc, row) => {
       row.forEach((e, i) => (acc[i] ? acc[i].push(e) : (acc[i] = [e])));
       return acc;
-    }, [])
-    .every(isValidRow);
+    }, []);
+    
+  for (let i = 0; i < columns.length; i++)
+      if (!isValidRow(columns[i])) return false;
+  
   const threeCols = board.reduce((acc, row) => {
     for (let i = 0; i < 3; i++)
       acc[i] ? acc[i].push(row.splice(0, 3)) : (acc[i] = [row.splice(0, 3)]);
     return acc;
   }, []);
 
-  for (let i = 0; i < threeCols.length; i++) {
+  for (let i = 0; i < threeCols.length; i++)
     while (threeCols[i].length !== 0)
-      if (!isValidRow([].concat(...threeCols[i].splice(0, 3)))) return false;
-  }
-
-  return rowsValid && colsValid;
+      if (!isValidRow([].concat(...threeCols[i].splice(0, 3)))) 
+          return false;
+    
+  return true;
 };
 
 const isValidRow = (row) =>
