19b3d85d32c9e4ed0cff6408a58ba575c3d9a2b4
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-12 00:06:42 +0530

    Time: 87 ms (30.88%), Space: 42.1 MB (41.91%) - LeetHub

--- 1078-occurrences-after-bigram/1078-occurrences-after-bigram.js
+++ 1078-occurrences-after-bigram/1078-occurrences-after-bigram.js
@@ -4,7 +4,7 @@
  * @param {string} second
  * @return {string[]}
  */
-var findOcurrences = function(text, first, second) {
+const findOcurrences = (text, first, second) => {
     const words = text.split(' ');
     return words.reduce((acc ,word, i) => {
         if (word === first && words[i + 1] === second && words[i + 2])
