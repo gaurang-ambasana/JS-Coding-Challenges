a02f138185ddfb8b2c17d5575dcd0e36b457d1a4
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-12 00:40:56 +0530

    Time: 148 ms (24.91%), Space: 49 MB (90.57%) - LeetHub

--- 344-reverse-string/344-reverse-string.js
+++ 344-reverse-string/344-reverse-string.js
@@ -2,4 +2,6 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
-const reverseString = s => s.reverse();
+const reverseString = (s) => {
+    s.reverse();
+}
