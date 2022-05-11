2c1395442f621c904dca0ac8642e21a16797174d
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-12 00:37:18 +0530

    Time: 71 ms (77.11%), Space: 44.3 MB (82.37%) - LeetHub

--- 541-reverse-string-ii/541-reverse-string-ii.js
+++ 541-reverse-string-ii/541-reverse-string-ii.js
@@ -3,4 +3,7 @@
  * @param {number} k
  * @return {string}
  */
-const reverseStr = (s, k) => s.match(new RegExp(`.{1,${k}}`, 'g')).map((s, i) => i % 2 === 0 ? s.split('').reverse().join('') : s).join('');
+const reverseStr = (s, k) => {
+    const regex = new RegExp(`.{1,${k}}`, 'g');
+    return s.match(regex).map((s, i) => i % 2 === 0 ? s.split('').reverse().join('') : s).join('');
+}
