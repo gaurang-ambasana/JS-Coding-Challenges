86080e616a2a17b46e826bb90b83ecbe8c995542
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-13 19:27:47 +0530

    Time: 55 ms (92.59%), Space: 41.9 MB (56.30%) - LeetHub

--- 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.js
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.js
@@ -4,6 +4,6 @@
  * @return {number}
  */
 const isPrefixOfWord = (sentence, searchWord) => {
-    const idx = sentence.split(' ').findIndex((word) => word.startsWith(searchWord));
+    const idx = sentence.split(' ').findIndex(word => word.startsWith(searchWord));
     return idx === -1 ? idx : idx + 1;
 };
