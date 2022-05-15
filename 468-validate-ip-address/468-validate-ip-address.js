94eec7430c1b396ab1e28f3b7fbb489dfe5aa0d5
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-15 23:48:53 +0530

    Time: 90 ms (29.53%), Space: 42.1 MB (56.38%) - LeetHub

--- 468-validate-ip-address/468-validate-ip-address.js
+++ 468-validate-ip-address/468-validate-ip-address.js
@@ -2,34 +2,37 @@
  * @param {string} queryIP
  * @return {string}
  */
-const validIPAddress = (queryIP) => {
-    const ipv4 = () => {
-        const x = queryIP.split('.');
-        if (x.length !== 4) return false;
-        for (let i = 0; i < x.length; i++) {
-            if (x[i] <= 255 && 0 <= x[i] && parseInt(x[i]).toString() === x[i])
-                continue;
-            return false;
-        }     
-        return true;
-    };
+const ipv4 = (IP) => {
+    const x = IP.split('.');
     
-    const ipv6 = () => {
-        const x = queryIP.split(':');
-        if (x.length !== 8) return false;
-        const allowed = '0123456789abcdef';
-        for (let i = 0; i < x.length; i++) {
-            if (1 <= x[i].length && 
-                x[i].length <= 4 &&
-                x[i].toLowerCase().split('')
-                .every(c => allowed.includes(c))) continue;
-            return false;
-        }     
-        return true;
-    }
+    if (x.length !== 4) return false;
     
-    const ip4 = ipv4();
-    const ip6 = ipv6();
+    for (let i = 0; i < x.length; i++) {
+        if (x[i] <= 255 && 0 <= x[i] && parseInt(x[i]).toString() === x[i])
+            continue;
+        
+        return false;
+    }     
     
-    return ip4 ? "IPv4" : ip6 ? "IPv6" : "Neither";
+    return true;
 };
+
+const ipv6 = (IP) => {
+    const x = IP.split(':');
+    
+    if (x.length !== 8) return false;
+        
+    const allowed = '0123456789abcdef';
+        
+    for (let i = 0; i < x.length; i++) {
+        if (1 <= x[i].length && x[i].length <= 4 && 
+            x[i].toLowerCase().split('')
+            .every(c => allowed.includes(c))) continue;
+        
+        return false;
+    }     
+    
+    return true;
+}
+
+const validIPAddress = (queryIP) => ipv4(queryIP) ? "IPv4" : ipv6(queryIP) ? "IPv6" : "Neither";
