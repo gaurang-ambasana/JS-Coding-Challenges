7199d223d6210663dd77be16ce59e84154d3e9e7
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-17 00:52:57 +0530

    Time: 139 ms (22.18%), Space: 44.6 MB (86.38%) - LeetHub

--- 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.js
+++ 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.js
@@ -3,4 +3,4 @@
  * @param {number} k
  * @return {string}
  */
-var kthDistinct = (arr, k) => arr.filter((c) => arr.indexOf(c) === arr.lastIndexOf(c))[k - 1] ?? '';
+const kthDistinct = (arr, k) => arr.filter((c) => arr.indexOf(c) === arr.lastIndexOf(c))[k - 1] ?? '';
