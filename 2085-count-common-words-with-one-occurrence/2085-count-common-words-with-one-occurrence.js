1694aad2c3fe7d2bba838fc5875595e5eb33e7e3
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-17 00:31:18 +0530

    Time: 125 ms (38.68%), Space: 47.5 MB (19.50%) - LeetHub

--- 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.js
+++ 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.js
@@ -9,5 +9,7 @@ const countWords = (words1, words2) => {
     const freqCounter1 = getFreqOfArr(words1);
     const freqCounter2 = getFreqOfArr(words2);
     
-    return Object.values(freqCounter1) > Object.values(freqCounter2) ? Object.keys(freqCounter1).filter(key => key in freqCounter2 && freqCounter1[key] === 1 && freqCounter2[key] === 1).length : Object.keys(freqCounter2).filter(key => key in freqCounter1 && freqCounter1[key] === 1 && freqCounter2[key] === 1).length;
+    return Object.values(freqCounter1) > Object.values(freqCounter2) ?
+        Object.keys(freqCounter1).filter(key => key in freqCounter2 && freqCounter1[key] === 1 && freqCounter2[key] === 1).length : 
+        Object.keys(freqCounter2).filter(key => key in freqCounter1 && freqCounter1[key] === 1 && freqCounter2[key] === 1).length;
 }
