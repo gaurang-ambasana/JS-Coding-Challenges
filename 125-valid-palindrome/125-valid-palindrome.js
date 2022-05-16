717e416b20a456e5f26b8ae02641e9e863826328
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-16 23:33:50 +0530

    Time: 94 ms (57.05%), Space: 46.2 MB (48.13%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -4,6 +4,5 @@
  */
 var isPalindrome = function(s) {
     const validS = s.replace(/[^0-9a-z]/gi, '').toLowerCase();
-    console.log(validS)
     return validS === validS.split('').reverse().join('');
 };
