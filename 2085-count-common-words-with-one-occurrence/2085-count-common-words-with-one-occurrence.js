e34935573ded8811eed619c64cdf7069f588e9d5
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-17 00:27:02 +0530

    Time: 101 ms (60.69%), Space: 47.3 MB (24.84%) - LeetHub

--- 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.js
+++ 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.js
@@ -3,19 +3,13 @@
  * @param {string[]} words2
  * @return {number}
  */
-var countWords = function(words1, words2) {
+const countWords = (words1, words2) => {
     const freqCounter1 = getFreqOfArr(words1);
     const freqCounter2 = getFreqOfArr(words2);
     
-    if (Object.values(freqCounter1) > Object.values(freqCounter2)) {
-        return Object.keys(freqCounter1).filter(key => {
-            if (key in freqCounter2 && freqCounter1[key] === 1 && freqCounter2[key] === 1) return key;
-        }).length;
-    } else {
-        return Object.keys(freqCounter2).filter(key => {
-            if (key in freqCounter1 && freqCounter1[key] === 1 && freqCounter2[key] === 1) return key;
-        }).length;
-    }
+    if (Object.values(freqCounter1) > Object.values(freqCounter2))
+        return Object.keys(freqCounter1).filter(key => key in freqCounter2 && freqCounter1[key] === 1 && freqCounter2[key] === 1).length;
+    else return Object.keys(freqCounter2).filter(key => key in freqCounter1 && freqCounter1[key] === 1 && freqCounter2[key] === 1).length;
 };
 
 const getFreqOfArr = (arr) => arr.reduce((freq, word) => (freq[word] = (freq[word] ?? 0) + 1, freq), {});
