c0dad0fbbc5f04f6c118016669e46bb3255bd295
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-16 23:55:25 +0530

    Time: 198 ms (5.20%), Space: 49.8 MB (56.15%) - LeetHub

--- 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.js
+++ 2108-find-first-palindromic-string-in-the-array/2108-find-first-palindromic-string-in-the-array.js
@@ -2,4 +2,4 @@
  * @param {string[]} words
  * @return {string}
  */
-const firstPalindrome = (words) => words.find((word) => word === word.split('').reverse().join('')) ?? '';
+const firstPalindrome = (words) => words.find(word => word === word.split('').reverse().join('')) ?? '';
