31ee5f273265141003e5d2f66de93f80a18fd2db
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-17 00:06:11 +0530

    Time: 128 ms (5.24%), Space: 42.4 MB (88.67%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.js
@@ -3,8 +3,6 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
-const intersection = function(nums1, nums2) {
-    return nums1.length > nums2.length ?
-        [...new Set(nums1.filter(num => nums2.includes(num)))] :
-        [...new Set(nums2.filter(num => nums1.includes(num)))];
-};
+const intersection = (nums1, nums2) => nums1.length > nums2.length ?
+      [...new Set(nums1.filter(num => nums2.includes(num)))] :
+      [...new Set(nums2.filter(num => nums1.includes(num)))];
