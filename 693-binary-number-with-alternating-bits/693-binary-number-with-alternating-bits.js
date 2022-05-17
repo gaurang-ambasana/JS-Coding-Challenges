7cc0b6f0626058411ad436978ab8710d001fe03f
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-17 17:53:45 +0530

    Time: 61 ms (84.21%), Space: 42.4 MB (9.77%) - LeetHub

--- 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.js
+++ 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.js
@@ -2,4 +2,4 @@
  * @param {number} n
  * @return {boolean}
  */
-const hasAlternatingBits = (n) => n.toString(2).split('').every((b, i, a) => b !== a[i + 1]);
+const hasAlternatingBits = n => n.toString(2).split('').every((b, i, a) => b !== a[i + 1]);
