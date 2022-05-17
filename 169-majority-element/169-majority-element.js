34ccff7883675e40aa0b2526be9d3818b19f72a5
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-17 16:57:32 +0530

    Time: 78 ms (70.83%), Space: 43.7 MB (62.95%) - LeetHub

--- 169-majority-element/169-majority-element.js
+++ 169-majority-element/169-majority-element.js
@@ -2,7 +2,7 @@
  * @param {number[]} nums
  * @return {number}
  */
-var majorityElement = function(nums) {
+const majorityElement = nums => {
     const freqCounter = nums.reduce((counter, n) => (counter[n] = (counter[n] ?? 0) + 1, counter), {});
     return Object.keys(freqCounter).find(key => freqCounter[key] > nums.length / 2);
 };
