4c7ca8a6cf775f28eb0177f471831420b108fe53
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-17 17:38:11 +0530

    Time: 77 ms (80.44%), Space: 43.9 MB (25.68%) - LeetHub

--- 342-power-of-four/342-power-of-four.js
+++ 342-power-of-four/342-power-of-four.js
@@ -2,4 +2,4 @@
  * @param {number} n
  * @return {boolean}
  */
-const isPowerOfFour = n => n === 1 ? true : n % 4 === 0 && n !== 0 ? isPowerOfFour(n / 4) : false;
+const isPowerOfFour = n => n === 1 ? true : n % 4 === 0 && n !== 0 ? isPowerOfFour(n/4) : false;
