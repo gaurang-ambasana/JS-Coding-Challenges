3d8dfd1be5cb0837e469d1510f5ff13527776145
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-18 19:12:47 +0530

    Time: 111 ms (25.52%), Space: 45.1 MB (15.06%) - LeetHub

--- 443-string-compression/443-string-compression.js
+++ 443-string-compression/443-string-compression.js
@@ -2,13 +2,15 @@
  * @param {character[]} chars
  * @return {number}
  */
-var compress = function(chars) {
+const compress = (chars) => {
     const arr = [chars[0]];
+    
     for (let i = 1; i < chars.length; i++) {
-        if (arr[arr.length - 1][0] !== chars[i]) {
+        const n = arr.length - 1;
+        if (arr[n][0] !== chars[i]) {
             arr.push(chars[i]);
-        } else arr[arr.length - 1] = arr[arr.length - 1].concat(chars[i]);
+        } else arr[n] = arr[n].concat(chars[i]);
     }
-    const res = arr.reduce((a, c) => c.length > 1 ? a.concat(c[0] + c.length) : a.concat(c), '');
-    chars.splice(0, chars.length, ...res.split(''))
+    
+    chars.splice(0, chars.length, ...arr.reduce((a, c) => c.length > 1 ? a.concat(c[0] + c.length) : a.concat(c), '').split(''))
 };
