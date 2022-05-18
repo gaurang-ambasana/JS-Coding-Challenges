20a189e5c687f9ac5efe7e4e7dd3e31938709b0f
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-18 17:22:47 +0530

    Time: 109 ms (8.56%), Space: 42.4 MB (15.28%) - LeetHub

--- 290-word-pattern/290-word-pattern.js
+++ 290-word-pattern/290-word-pattern.js
@@ -3,16 +3,20 @@
  * @param {string} s
  * @return {boolean}
  */
-const wordPattern = function(pattern, s) {
+const wordPattern = (pattern, s) => {
     const sArr = s.split(' ');
+    
     if (pattern.length !== sArr.length) return false;
     if (new Set(pattern.split('')).size !== new Set(sArr).size) return false;
+    
     const hash = {};
+    
     for (let i = 0; i < sArr.length && i < pattern.length; i++) {
         if (pattern[i] in hash) {
             if (hash[pattern[i]] !== sArr[i])
                 return false;
         } else hash[pattern[i]] = sArr[i];
     }
+    
     return true;
 };
