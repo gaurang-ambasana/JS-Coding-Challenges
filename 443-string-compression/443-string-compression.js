917c7451c20e34a0750ab7073f06f46190c620ed
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-18 19:13:52 +0530

    Time: 110 ms (26.57%), Space: 44.9 MB (18.41%) - LeetHub

--- 443-string-compression/443-string-compression.js
+++ 443-string-compression/443-string-compression.js
@@ -2,14 +2,13 @@
  * @param {character[]} chars
  * @return {number}
  */
-const compress = (chars) => {
+const compress = chars => {
     const arr = [chars[0]];
     
     for (let i = 1; i < chars.length; i++) {
         const n = arr.length - 1;
-        if (arr[n][0] !== chars[i]) {
-            arr.push(chars[i]);
-        } else arr[n] = arr[n].concat(chars[i]);
+        if (arr[n][0] !== chars[i]) arr.push(chars[i]);
+        else arr[n] = arr[n].concat(chars[i]);
     }
     
     chars.splice(0, chars.length, ...arr.reduce((a, c) => c.length > 1 ? a.concat(c[0] + c.length) : a.concat(c), '').split(''))
