9c353df970ace6e01da9e446cb6a808517b035f1
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-18 17:22:20 +0530

    Time: 77 ms (49.39%), Space: 42.2 MB (37.53%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} pattern
+ * @param {string} s
+ * @return {boolean}
+ */
+const wordPattern = function(pattern, s) {
+    const sArr = s.split(' ');
+    if (pattern.length !== sArr.length) return false;
+    if (new Set(pattern.split('')).size !== new Set(sArr).size) return false;
+    const hash = {};
+    for (let i = 0; i < sArr.length && i < pattern.length; i++) {
+        if (pattern[i] in hash) {
+            if (hash[pattern[i]] !== sArr[i])
+                return false;
+        } else hash[pattern[i]] = sArr[i];
+    }
+    return true;
+};
