4fb6e6d47d5e2e9a28e4f21d3376c0b75dbc370f
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-19 15:29:12 +0530

    Time: 103 ms (20.42%), Space: 43.7 MB (27.38%) - LeetHub

--- 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.js
+++ 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.js
@@ -6,13 +6,15 @@
 const removeDigit = (number, digit) => {
     let max = '0';
     const nums = number.split('');
+    
     for (let i = 0; i < nums.length; i++) {
         const n = nums[i];
         const temp = [...number];
         if (n === digit) {
             temp.splice(i, 1);
             const curr = temp.join('')
-            if (curr > max) max = curr;
+            if (curr > max)
+                max = curr;
         }
     }
     
