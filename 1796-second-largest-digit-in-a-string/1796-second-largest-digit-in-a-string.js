a8bcfa51edd940b953fb5d860be09b2c584678d4
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-19 15:01:05 +0530

    Time: 73 ms (83.47%), Space: 44.2 MB (51.24%) - LeetHub

--- 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.js
+++ 1796-second-largest-digit-in-a-string/1796-second-largest-digit-in-a-string.js
@@ -2,7 +2,7 @@
  * @param {string} s
  * @return {number}
  */
-var secondHighest = function(s) {
+const secondHighest = (s) => {
     const numsInS = s.match(/\d/g);
     const nums = numsInS ? Array.from(new Set(numsInS.join('').split(''))).sort((a, b) => b - a) : [];
     return nums.length >= 2 ? nums[1] : -1;
