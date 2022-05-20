a592db2d3c0d0c132c8d9edd8412e1ed02172bb4
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-20 23:38:15 +0530

    Time: 1673 ms (5.25%), Space: 98.2 MB (5.25%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.js
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.js
@@ -8,18 +8,19 @@ var minimumRounds = function(tasks) {
     
     if (!Object.values(taskCounter).every(c => c > 1)) return -1;
     
-    while (Object.values(taskCounter).some(c => c > 0)) {
+    while (Object.values(taskCounter).find(c => c > 0)) {
         for (const key in taskCounter) {
             if (taskCounter[key] !== 0) {
                 if (taskCounter[key] % 3 === 0) {
                     taskCounter[key] -= 3;
                     rounds++;
-                } else{ 
+                } else { 
                     taskCounter[key] -= 2;
                     rounds++;
                 }
             }
         }
+        console.log(taskCounter)
     }    
     
     return rounds;
