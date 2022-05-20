0322ebaef30d4135c2e6a926e00bf42d89601ec3
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-21 00:35:42 +0530

    Time: 155 ms (5.56%), Space: 43.8 MB (99.72%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.js
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.js
@@ -2,7 +2,7 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-const frequencySort = (nums) => {
+const frequencySort = nums => {
     const counter = nums.reduce((c, a) => (c[a] = (c[a] ?? 0) + 1, c), {});
     return nums.sort((a, b) => {
         const diff = counter[a] - counter[b] 
