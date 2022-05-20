e0df8accd7aa98a6a378dafbba307b635ef3cd67
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-21 00:45:02 +0530

    Time: 102 ms (33.66%), Space: 44.5 MB (64.05%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.js
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.js
@@ -2,6 +2,4 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-var divideArray = function(nums) {
-    return Object.values(nums.reduce((c, a) => (c[a] = (c[a] ?? 0) + 1, c), {})).every(val => val % (nums.length / (nums.length / 2)) === 0);
-};
+const divideArray = nums => Object.values(nums.reduce((c, a) => (c[a] = (c[a] ?? 0) + 1, c), {})).every(val => val % 2 === 0);
