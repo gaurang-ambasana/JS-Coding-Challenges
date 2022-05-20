e8ad04a5ef838c58133b6bf340c980db3c3a4d39
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-20 23:56:24 +0530

    Time: 266 ms (20.66%), Space: 59.3 MB (56.72%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.js
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.js
@@ -7,20 +7,13 @@ var minimumRounds = function(tasks) {
     let rounds = 0;
     const taskCounter = tasks.reduce((counter, d) => (counter[d] = (counter[d] ?? 0) + 1, counter), {});
     
-    if (Object.values(taskCounter).find(c => c < 2)) return -1;
-    
-    while (Object.values(taskCounter).some(c => c > 0)) {
-        for (const key in taskCounter) {
-            if (taskCounter[key] !== 0) {
-                if (taskCounter[key] % 3 === 0) {
-                    taskCounter[key] -= 3;
-                    ++rounds;
-                } else { 
-                    taskCounter[key] -= 2;
-                    ++rounds;
-                }
-            }
-        }
+    for (const val of Object.values(taskCounter)) {
+        if (val < 2) return -1;
+        
+        const count = Math.floor(val / 3);
+        
+        if (val % 3 !== 0) rounds += count + 1;
+        else rounds += count;
     }    
     
     return rounds;
