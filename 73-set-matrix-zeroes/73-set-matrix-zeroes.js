2cd360135e555ceaa181f098c2ea91e969cfde25
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-23 23:35:28 +0530

    Time: 112 ms (47.99%), Space: 44.9 MB (56.17%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.js
@@ -2,14 +2,14 @@
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
  */
-const setZeroes = matrix => {
-    const makeRowZero = row => {
+const setZeroes = (matrix) => {
+    const makeRowZero = (row) => {
         for (let i = 0; i < matrix[row].length; i++)
             if (matrix[row][i] !== 0 && matrix[row][i] !== 's')
                 matrix[row][i] = 's';
     };
     
-    const makeColZero = col => {
+    const makeColZero = (col) => {
         for (let i = 0; i < matrix.length; i++)
             if (matrix[i][col] !== 0 && matrix[i][col] !== 's')
                 matrix[i][col] = 's';
