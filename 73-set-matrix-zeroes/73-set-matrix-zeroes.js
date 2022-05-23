521d7a9b69458a1d20dd18a8542efb01e119980d
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-23 18:24:34 +0530

    Time: 125 ms (32.92%), Space: 44.9 MB (66.32%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+const setZeroes = function(matrix) {
+    const makeRowZero = (row) => {
+        for (let i = 0; i < matrix[row].length; i++)
+            if (matrix[row][i] !== 0)
+                matrix[row][i] = 's';
+    };
+    
+    const makeColZero = (col) => {
+        for (let i = 0; i < matrix.length; i++)
+            if (matrix[i][col] !== 0)
+                matrix[i][col] = 's';
+    };
+    
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (matrix[i][j] === 0) {
+                makeRowZero(i);
+                makeColZero(j);
+            }
+        }
+    }
+    
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++)
+            if (matrix[i][j] === 's')
+                matrix[i][j] = 0;
+    }
+};
