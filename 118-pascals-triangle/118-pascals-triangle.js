b7d0f9f4b07cc31b3472d59b63003196bb7b7432
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-23 23:15:20 +0530

    Time: 65 ms (75.83%), Space: 42.1 MB (55.76%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.js
+++ 118-pascals-triangle/118-pascals-triangle.js
@@ -2,14 +2,17 @@
  * @param {number} numRows
  * @return {number[][]}
  */
-const generate = (numRows) => {
+const generate = numRows => {
     const triangle = [];
+    
     for (let i = 0; i < numRows; i++) {
         const temp = [];
+        
         for (let j = 0; j <= i; j++) {
             if (j === 0 || i === j) temp.push(1);
             else temp.push(triangle[i - 1][j] + triangle[i - 1][j - 1]);
         }
+        
         triangle.push(temp);
     }
     
