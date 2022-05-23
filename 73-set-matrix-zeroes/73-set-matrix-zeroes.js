349968c58056b14829ca5fe7284979a4c151bbe7
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-23 18:30:15 +0530

    Time: 174 ms (6.03%), Space: 44.4 MB (94.59%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.js
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.js
@@ -5,13 +5,13 @@
 const setZeroes = (matrix) => {
     const makeRowZero = (row) => {
         for (let i = 0; i < matrix[row].length; i++)
-            if (matrix[row][i] !== 0)
+            if (matrix[row][i] !== 0 && matrix[row][i] !== 's')
                 matrix[row][i] = 's';
     };
     
     const makeColZero = (col) => {
         for (let i = 0; i < matrix.length; i++)
-            if (matrix[i][col] !== 0)
+            if (matrix[i][col] !== 0 && matrix[i][col] !== 's')
                 matrix[i][col] = 's';
     };
     
