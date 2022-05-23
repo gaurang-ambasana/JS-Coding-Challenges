8ea01335e3911dfc28d657fe21730cae0a04080f
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-24 00:31:46 +0530

    Time: 73 ms (63.94%), Space: 42.2 MB (46.95%) - LeetHub

--- 75-sort-colors/75-sort-colors.js
+++ 75-sort-colors/75-sort-colors.js
@@ -2,7 +2,7 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-const sortColors = (nums) => {
+const sortColors = nums => {
     const counter = nums.reduce((c, n) => (c[n] = (c[n] ?? 0) + 1, c), {});
     let num = 0;
     nums.length = 0;
