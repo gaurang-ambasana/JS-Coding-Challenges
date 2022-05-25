f9095dab0c3bf2986f635e9e4ed8a5c56d2a0332
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-25 17:18:11 +0530

    Time: 113 ms (21.83%), Space: 47.8 MB (9.77%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.js
+++ 496-next-greater-element-i/496-next-greater-element-i.js
@@ -3,8 +3,8 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
-const nextGreaterElement = (nums1, nums2) => nums1.map(n => {
+const nextGreaterElement = (nums1, nums2) => nums1.map((n) => {
     const start = nums2.indexOf(n);
-    const greaterElm = nums2.slice(start).find(e => e > n);
+    const greaterElm = nums2.slice(start).find((e) => e > n);
     return greaterElm ?? -1;
 });
