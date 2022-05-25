ff9e0016da87f3efee77c62fbc7e52cceacec0c4
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-25 17:05:11 +0530

    Time: 81 ms (66.37%), Space: 43.6 MB (54.73%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.js
+++ 229-majority-element-ii/229-majority-element-ii.js
@@ -2,9 +2,7 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var majorityElement = function(nums) {
-    return Object.entries(nums.reduce((c, n) => (c[n] = (c[n] ?? 0) + 1, c), {})).reduce((acc ,[key, val]) => {
+const majorityElement = nums => Object.entries(nums.reduce((c, n) => (c[n] = (c[n] ?? 0) + 1, c), {})).reduce((acc ,[key, val]) => {
         if (val > nums.length / 3) acc.push(key);
         return acc;
-    }, [])
-};
+    }, []);
