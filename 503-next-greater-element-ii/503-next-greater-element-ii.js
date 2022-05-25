7b64041f0de790cc7757bfd0b35c40880f5dcafc
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-25 18:20:39 +0530

    Time: 630 ms (6.62%), Space: 59 MB (5.20%) - LeetHub

--- 503-next-greater-element-ii/503-next-greater-element-ii.js
+++ 503-next-greater-element-ii/503-next-greater-element-ii.js
@@ -2,4 +2,4 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-const nextGreaterElements = (nums) => nums.map((n, i) => i !== nums.length ? (nums.slice(i).find((m) => m > n) ?? (nums.find((m) => m > n) ?? -1)) : (nums.find((m) => m > n) ?? -1));
+const nextGreaterElements = nums => nums.map((n, i) => i !== nums.length ? (nums.slice(i).find((m) => m > n) ?? (nums.find((m) => m > n) ?? -1)) : (nums.find((m) => m > n) ?? -1));
