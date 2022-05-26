b4942ba0b43f48a76d02d9125684493fbf8d85a7
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-26 23:53:23 +0530

    Time: 97 ms (22.90%), Space: 42.4 MB (30.73%) - LeetHub

---
+++ 48-rotate-image/48-rotate-image.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+const rotate = (matrix) => {
+    const clone = [...matrix].reverse();
+    
+    for (let i = 0; i < matrix.length; i++) {
+        matrix[i] = clone.reduce((arr, row) => {
+            arr.push(row[i]);
+            return arr;
+        }, []);
+    }
+};
