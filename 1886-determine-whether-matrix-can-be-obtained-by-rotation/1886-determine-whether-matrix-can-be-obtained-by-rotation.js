f967a5b5a4b1a84a8a38c310481a0fd02712c2d9
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-27 00:31:34 +0530

    Time: 80 ms (49.18%), Space: 43.6 MB (30.33%) - LeetHub

---
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[][]} mat
+ * @param {number[][]} target
+ * @return {boolean}
+ */
+const findRotation = (mat, target) => {
+    const compare = () => mat.every((row, i) => row.every((val, j) => val === target[i][j]));
+    
+    const rotate = () => {
+        const clone = [...mat].reverse();
+    
+        for (let i = 0; i < mat.length; i++) {
+            mat[i] = clone.reduce((arr, row) => {
+                arr.push(row[i]);
+                return arr;
+            }, []);
+        }
+    }
+    
+    rotate();
+    
+    for (let i = 0; i < 4; i++) {
+        if (compare(mat)) return true;
+        rotate();
+    }
+    
+    return false;
+};
