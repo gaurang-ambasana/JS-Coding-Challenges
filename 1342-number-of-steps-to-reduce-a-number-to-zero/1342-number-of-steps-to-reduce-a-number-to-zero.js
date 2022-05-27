237736ee33fca6e1cd26426413ad62c625618c3f
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-27 11:25:08 +0530

    Time: 58 ms (93.34%), Space: 42.3 MB (29.16%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -4,14 +4,13 @@
  * @return {number}
  */
 const numberOfSteps = (num, steps = 0) => {
-    if (num !== 0) {
-        if (num % 2 === 0) {
-            steps++;
-            return numberOfSteps(num / 2, steps);
-        } else {
-            steps++;
-            return numberOfSteps(num - 1, steps);
-        }
+    if (num === 0) return steps;
+    
+    if (num % 2 === 0) {
+        steps++;
+        return numberOfSteps(num / 2, steps);
+    } else {
+        steps++;
+        return numberOfSteps(num - 1, steps);
     }
-    return steps;
 };
