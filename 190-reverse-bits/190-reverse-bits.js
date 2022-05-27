2e65a8c9c4015cf4a2bda90c529c4c3bd09d6d3b
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-27 12:08:41 +0530

    Time: 84 ms (59.04%), Space: 44.4 MB (20.20%) - LeetHub

---
+++ 190-reverse-bits/190-reverse-bits.js
@@ -0,0 +1,5 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number} - a positive integer
+ */
+const reverseBits = n => parseInt(n.toString(2).padStart(32, '0').split('').reverse().join(''), 2);
