830734785c773331711a26a6e9e2c59d088f4dac
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-27 11:18:02 +0530

    Time: 93 ms (26.38%), Space: 42.5 MB (5.03%) - LeetHub

---
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} num
+ * @return {number}
+ */
+const numberOfSteps = (num, steps = 0) => {
+    if (num !== 0) {
+        if (num % 2 === 0) {
+            steps++;
+            return numberOfSteps(num / 2, steps);
+        } else {
+            steps++;
+            return numberOfSteps(num - 1, steps);
+        }
+    }
+    
+    return steps;
+};
