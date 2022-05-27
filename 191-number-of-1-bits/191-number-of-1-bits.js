790b8880e9ed9d83c23563bc1302da201254e291
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-27 11:27:46 +0530

    Time: 88 ms (49.05%), Space: 43.7 MB (19.03%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.js
+++ 191-number-of-1-bits/191-number-of-1-bits.js
@@ -2,4 +2,4 @@
  * @param {number} n - a positive integer
  * @return {number}
  */
-const hammingWeight = (n) => n.toString(2).split('').reduce((counter, d) => (counter[d] = (counter[d] ?? 0) + 1, counter), {})['1'] ?? 0;
+const hammingWeight = n => n.toString(2).split('').reduce((counter, d) => (counter[d] = (counter[d] ?? 0) + 1, counter), {})['1'] ?? 0;
