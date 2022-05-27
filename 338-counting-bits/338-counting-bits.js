bf4982c22f1744923cdc6d3c88c41a4cc8fcd56d
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-27 11:57:36 +0530

    Time: 136 ms (41.15%), Space: 54.5 MB (10.14%) - LeetHub

--- 338-counting-bits/338-counting-bits.js
+++ 338-counting-bits/338-counting-bits.js
@@ -2,4 +2,4 @@
  * @param {number} n
  * @return {number[]}
  */
-const countBits = n => [...Array(n + 1).keys()].map(b => b.toString(2).split('').reduce((count, num) => num === '1' ? ++count : count, 0));
+const countBits = n => [...Array(n + 1).keys()].map((b) => b.toString(2).split('').reduce((count, num) => num === '1' ? ++count : count, 0));
