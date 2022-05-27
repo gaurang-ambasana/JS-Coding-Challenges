d84b90cbb964c898a47b90e1e3ebead8ee380656
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-27 12:14:01 +0530

    Time: 75 ms (52.24%), Space: 42.5 MB (8.96%) - LeetHub

---
+++ 2119-a-number-after-a-double-reversal/2119-a-number-after-a-double-reversal.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} num
+ * @return {boolean}
+ */
+var isSameAfterReversals = function(num) {
+    const reverseNum = n => parseInt(n.toString().split('').reverse().join(''));
+    
+    return num === reverseNum(reverseNum(num));
+};
