0fcce8295d832bdad3052c4b85ccb622b314be30
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-28 15:46:16 +0530

    Time: 149 ms (5.43%), Space: 42 MB (91.99%) - LeetHub

---
+++ 771-jewels-and-stones/771-jewels-and-stones.js
@@ -0,0 +1,6 @@
+/**
+ * @param {string} jewels
+ * @param {string} stones
+ * @return {number}
+ */
+const numJewelsInStones = (jewels, stones) => stones.split(``).reduce((num, s) => jewels.includes(s) ? ++num : num, 0);
