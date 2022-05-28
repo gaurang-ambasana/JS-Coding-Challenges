877a3628013754696d98c3394281aa3a459dc49f
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-28 15:41:30 +0530

    Time: 111 ms (16.84%), Space: 44.2 MB (55.42%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.js
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.js
@@ -2,4 +2,4 @@
  * @param {string} s
  * @return {string}
  */
-var reverseWords = s => s.trim().split(` `).filter(c => c.trim().length > 0).reverse().join(` `);
+const reverseWords = s => s.trim().split(' ').filter(c => c.trim().length > 0).reverse().join(' ');
