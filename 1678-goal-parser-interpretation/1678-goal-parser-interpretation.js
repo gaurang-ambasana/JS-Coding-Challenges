6d8a4ce5bad9499bf945906456a3f969a574a2fd
Gaurang Ambasana <gpambasana@gmail.com>
2022-05-28 15:22:08 +0530

    Time: 108 ms (9.31%), Space: 42.2 MB (37.23%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.js
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.js
@@ -2,4 +2,4 @@
  * @param {string} command
  * @return {string}
  */
-const interpret = (command) => command.replaceAll('()', 'o').replaceAll('(al)', 'al');
+const interpret = (command) => command.replaceAll('()','o').replaceAll('(al)','al');
