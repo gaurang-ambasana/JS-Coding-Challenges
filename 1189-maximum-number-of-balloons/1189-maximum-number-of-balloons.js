6ca17e54c42d1fee9eb93756dca3afb7c057745a
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-01 00:30:09 +0530

    Time: 130 ms (8.36%), Space: 44.3 MB (25.75%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.js
@@ -0,0 +1,47 @@
+/**
+ * @param {string} text
+ * @return {number}
+ */
+const maxNumberOfBalloons = function(text) {
+    const balloon = {
+        'b' : 0,
+        'a' : 0,
+        'l' : 0,
+        'o' : 0,
+        'n' : 0,
+    };
+    
+    for (const char of text) {
+        switch (char) {
+            case 'b' : {
+                ++balloon['b'];
+                break;
+            }
+                
+            case 'a' : {
+                ++balloon['a'];
+                break;
+            }
+                
+            case 'l' : { 
+                ++balloon['l'];
+                break;
+            }
+                
+            case 'o' : {
+                ++balloon['o'];
+                break;
+            }
+                
+            case 'n' : { 
+                ++balloon['n'];
+                break;
+            }
+        }
+    }
+    
+    balloon['l'] = Math.floor(balloon['l'] / 2);
+    balloon['o'] = Math.floor(balloon['o'] / 2);
+    
+    return Math.min(...Object.values(balloon))
+};
