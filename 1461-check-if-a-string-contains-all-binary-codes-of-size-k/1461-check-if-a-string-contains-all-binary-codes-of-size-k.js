733905089fc685fd834fd76eed96215dfc55ea9b
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-01 00:45:54 +0530

    Time: 295 ms (77.78%), Space: 63.1 MB (61.11%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.js
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.js
@@ -3,12 +3,12 @@
  * @param {number} k
  * @return {boolean}
  */
-const hasAllCodes = function(s, k) {
+const hasAllCodes = (s, k) => {
     if (k === 0) return true;
     
     if (s.length < k) return false;
     
-    const max = 2 ** k;
+    const max = Math.pow(2, k);
     const set = new Set();
 
     for (let i = 0, n = s.length - k; i <= n; i++) {
