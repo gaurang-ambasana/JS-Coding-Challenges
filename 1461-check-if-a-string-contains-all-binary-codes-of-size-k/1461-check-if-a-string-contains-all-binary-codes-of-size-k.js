18ec55cea69fa33576b7cb314556190e9b48ead4
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-01 00:52:37 +0530

    Time: 398 ms (38.89%), Space: 66.7 MB (22.22%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.js
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.js
@@ -6,7 +6,7 @@
 let hasAllCodes = (s, k) => {
     const set = new Set();
 
-    for (let i = 0; i <= s.length - k; i++)
+    for (let i = 0, n = s.length - k; i <= n; i++)
         set.add(s.substr(i, k));
 
     return set.size === Math.pow(2, k);
