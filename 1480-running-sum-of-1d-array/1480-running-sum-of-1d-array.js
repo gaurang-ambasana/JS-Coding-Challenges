c8c4b3c798b202cda58ecb7cc9ff2f9579613c30
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-01 13:09:58 +0530

    Time: 96 ms (28.26%), Space: 42.4 MB (50.63%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const runningSum = (nums) => {
+    const res = [nums[0]];
+    
+    for (let i = 1; i < nums.length; i++)
+        res.push(nums[i] + res[i - 1]);
+    
+    return res;
+};
