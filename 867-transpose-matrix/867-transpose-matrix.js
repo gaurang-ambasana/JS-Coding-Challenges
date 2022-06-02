798070fbdc6db834c63b4da9f028b8c3f6d25319
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-02 18:20:25 +0530

    Time: 62 ms (100.00%), Space: 44.4 MB (84.06%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.js
+++ 867-transpose-matrix/867-transpose-matrix.js
@@ -3,21 +3,20 @@
  * @return {number[][]}
  */
 const transpose = (matrix) => {
-  const clone = matrix.map((e) => [...e]); // deep clone
   const res = [];
   const n = matrix.length;
   const m = matrix[0].length;
 
   if (n === m) {
     for (let i = 0; i < n; i++) {
-      res[i] = clone.reduce((arr, row) => {
+      res[i] = matrix.reduce((arr, row) => {
         arr.push(row[i]);
         return arr;
       }, []);
     }
   } else {
     for (let i = 0; i < m; i++) {
-      res[i] = clone.reduce((arr, row) => {
+      res[i] = matrix.reduce((arr, row) => {
         arr.push(row[i]);
         return arr;
       }, []);
