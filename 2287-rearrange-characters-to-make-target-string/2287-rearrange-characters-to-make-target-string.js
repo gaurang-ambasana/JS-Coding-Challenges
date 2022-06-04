48a843cbb5562544fac956b5abfd6dc395d66dda
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-04 19:13:53 +0530

    Time: 71 ms (88.85%), Space: 42.8 MB (35.97%) - LeetHub

---
+++ 2287-rearrange-characters-to-make-target-string/2287-rearrange-characters-to-make-target-string.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @param {string} target
+ * @return {number}
+ */
+var rearrangeCharacters = function(s, target) {
+    const freqCounter = str => str.split('').reduce((c, a) => (c[a] = (c[a] ?? 0) + 1, c), {});
+                                                    
+    const targetFreq = freqCounter(target);
+    const sFreq = freqCounter(s);
+    
+    const max = Object.keys(targetFreq).reduce((min, key) => Math.min(Math.floor(sFreq[key] / targetFreq[key]), min), Infinity);
+    
+    return isNaN(max) ? 0 : max;
+};
