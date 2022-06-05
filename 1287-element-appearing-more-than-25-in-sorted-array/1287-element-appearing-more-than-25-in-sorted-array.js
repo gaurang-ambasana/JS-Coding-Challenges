e850f9a063eb5c227e193598ad899d15140134d7
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-05 11:40:59 +0530

    Time: 99 ms (43.61%), Space: 49.4 MB (12.03%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.js
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.js
@@ -2,8 +2,8 @@
  * @param {number[]} arr
  * @return {number}
  */
-const findSpecialInteger = arr => {
+const findSpecialInteger = (arr) => {
     const required = arr.length * 0.25;
     const freqOfArr = arr.reduce((c, e) => (c[e] = (c[e] ?? 0) + 1, c), {});
-    return Object.keys(freqOfArr).find(e => freqOfArr[e] > required);
+    return Object.keys(freqOfArr).find((e) => freqOfArr[e] > required);
 };
