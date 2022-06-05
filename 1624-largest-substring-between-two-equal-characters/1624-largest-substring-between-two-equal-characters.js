f04a9ff5110322dafe1ec44d0b3a6373d0b5c1ec
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-05 12:12:44 +0530

    Time: 107 ms (20.35%), Space: 43.2 MB (28.32%) - LeetHub

--- 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.js
+++ 1624-largest-substring-between-two-equal-characters/1624-largest-substring-between-two-equal-characters.js
@@ -2,7 +2,7 @@
  * @param {string} s
  * @return {number}
  */
-const maxLengthBetweenEqualCharacters = (s) => {
+const maxLengthBetweenEqualCharacters = s => {
     const map = s.split('').reduce((o, c, i) => {
         if (c in o) o[c].push(i);
         else o[c] = [i];
@@ -11,12 +11,14 @@ const maxLengthBetweenEqualCharacters = (s) => {
     
     return Object.keys(map).reduce((max, key) => {
         const val = map[key];
+        
         if (val.length > 1) {
             const start = Math.min(...val) + 1;
             const end = Math.max(...val);
             const len = s.substring(start, end).length;
-            max = Math.max(max, len);
+            return Math.max(max, len);
         }
+        
         return max;
     }, -1);
 };
