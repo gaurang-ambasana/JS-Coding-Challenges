a585c7a4669f7d67ff07782176958bcd9bbd2f4d
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-06 18:42:53 +0530

    Time: 66 ms (82.21%), Space: 41.4 MB (99.83%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.js
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.js
@@ -2,5 +2,5 @@
  * @param {number[][]} accounts
  * @return {number}
  */
-const maximumWealth = accounts => accounts.reduce((max, ele) => Math.max(max, ele.reduce((sum, ele) => sum + ele, 0)), 0);
+const maximumWealth = (accounts) => accounts.reduce((max, ele) => Math.max(max, ele.reduce((sum, ele) => sum += ele, 0)), 0);
                                 
