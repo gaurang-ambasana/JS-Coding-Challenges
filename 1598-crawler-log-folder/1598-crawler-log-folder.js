a2aded2d22d0bc4b1412f9bc7387706df6b1b5e2
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-07 23:00:02 +0530

    Time: 104 ms (23.68%), Space: 42.4 MB (68.42%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.js
+++ 1598-crawler-log-folder/1598-crawler-log-folder.js
@@ -2,20 +2,18 @@
  * @param {string[]} logs
  * @return {number}
  */
-var minOperations = function(logs) {
-    return logs.reduce((steps, l, i) => {
-        switch (l) {
-            case '../': {
-                if (steps > 0) --steps;
-                return steps;
-            }
+const minOperations = (logs) => logs.reduce((steps, l, i) => {
+    switch (l) {
+        case '../': {
+            if (steps > 0) --steps;
+            return steps;
+         }
             
-            case './': return steps;
+         case './': return steps;
                 
-            default : {
-                ++steps;
-                return steps;
-            }
+         default : {
+            ++steps;
+            return steps;
         }
-    }, 0);
-};
+    }
+}, 0);
