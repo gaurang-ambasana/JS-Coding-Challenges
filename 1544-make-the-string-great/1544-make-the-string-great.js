e184e9b080cbd2d1f8c09c9a0912663c055369a6
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-15 00:21:39 +0530

    Time: 81 ms (69.75%), Space: 44.9 MB (14.29%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.js
+++ 1544-make-the-string-great/1544-make-the-string-great.js
@@ -3,10 +3,10 @@
  * @return {string}
  */
 const makeGood = (s) => {
-    const stack = [s[0]];
+    let stack = `${s[0]}`;
     
     for (let i = 1; i < s.length; i++)
-        (s[i] === s[i].toUpperCase() && (s[i].toLowerCase() === stack[stack.length - 1])) || (s[i] === s[i].toLowerCase() && (s[i].toUpperCase() === stack[stack.length - 1])) ? stack.pop() : stack.push(s[i]);
+        (s[i] === s[i].toUpperCase() && (s[i].toLowerCase() === stack[stack.length - 1])) || (s[i] === s[i].toLowerCase() && (s[i].toUpperCase() === stack[stack.length - 1])) ? (stack = stack.slice(0, -1)) : (stack = stack.concat(s[i]));
     
-    return stack.join(``);
+    return stack;
 };
