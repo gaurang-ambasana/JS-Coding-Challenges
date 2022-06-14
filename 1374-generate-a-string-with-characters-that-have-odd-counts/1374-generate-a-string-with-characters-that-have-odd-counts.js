a3bfc0083f0772bb75b0ff81cc913816e49e9ba0
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-14 18:52:55 +0530

    Time: 128 ms (5.39%), Space: 42.3 MB (61.08%) - LeetHub

--- 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.js
+++ 1374-generate-a-string-with-characters-that-have-odd-counts/1374-generate-a-string-with-characters-that-have-odd-counts.js
@@ -2,4 +2,4 @@
  * @param {number} n
  * @return {string}
  */
-const generateTheString = (n) => `a`.repeat(n - 1) + (n % 2 ? `a` : `b`)
+const generateTheString = (n) => `a`.repeat(n - 1) + (n % 2 ? `a` : `b`);
