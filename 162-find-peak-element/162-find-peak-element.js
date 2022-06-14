557b0728175b9c754f9edfd62eb487104ef9a711
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-15 01:16:15 +0530

    Time: 93 ms (28.30%), Space: 42.1 MB (55.81%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.js
+++ 162-find-peak-element/162-find-peak-element.js
@@ -2,13 +2,15 @@
  * @param {number[]} nums
  * @return {number}
  */
-const findPeakElement = (nums) => {
+const findPeakElement = nums => {
     const map = nums.reduce((o, n, i) => (o[i] = n, o), {
         "-1": -Infinity,
         [nums.length]: -Infinity, 
     });
     
-    for (let i = 0; i < nums.length; i++)
-        if (map[i] > map[i - 1] && map[i] > map[i + 1])
-            return i;
+    return nums.findIndex((_, i) => map[i] > map[i + 1] && map[i] && map[i - 1]);
+    
+    // for (let i = 0; i < nums.length; i++)
+    //     if (map[i] > map[i - 1] && map[i] > map[i + 1])
+    //         return i;
 };
