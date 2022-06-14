a8eeee102fc54cbcd92214e3a1698e79d7951574
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-14 18:44:02 +0530

    Time: 71 ms (82.15%), Space: 43.8 MB (87.87%) - LeetHub

--- 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
@@ -2,4 +2,4 @@
  * @param {string[]} sentences
  * @return {number}
  */
-const mostWordsFound = (sentences) => sentences.reduce((max, s) => Math.max(max, s.split(' ').length), 0);
+const mostWordsFound = sentences => sentences.reduce((max, s) => Math.max(max, s.split(' ').length), 0);
