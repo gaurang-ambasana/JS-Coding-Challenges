b2e21bd2cbe5857fb3883ddc9738228e2ba9f784
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-14 18:49:50 +0530

    Time: 140 ms (18.29%), Space: 45.6 MB (56.10%) - LeetHub

--- 2047-number-of-valid-words-in-a-sentence/2047-number-of-valid-words-in-a-sentence.js
+++ 2047-number-of-valid-words-in-a-sentence/2047-number-of-valid-words-in-a-sentence.js
@@ -2,4 +2,4 @@
  * @param {string} sentence
  * @return {number}
  */
-const countValidWords = sentence => sentence.split(/\s+/).filter(s => s.match(/^[a-z]+(-[a-z]+)?[\.!,]?$/) || s.match(/^[\.!,]$/)).length;
+const countValidWords = (sentence) => sentence.split(/\s+/).filter(s => s.match(/^[a-z]+(-[a-z]+)?[\.!,]?$/) || s.match(/^[\.!,]$/)).length;
