bd7d0b516fce0164ea5d25b9e7a34e069702815f
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-17 16:22:10 +0530

    Time: 116 ms (26.13%), Space: 44.6 MB (35.19%) - LeetHub

--- 2303-calculate-amount-paid-in-taxes/2303-calculate-amount-paid-in-taxes.js
+++ 2303-calculate-amount-paid-in-taxes/2303-calculate-amount-paid-in-taxes.js
@@ -4,18 +4,11 @@
  * @return {number}
  */
 const calculateTax = (brackets, income) => {
-    let ans = 0;
-    let left = income;
-    let prev = 0;
-    
-    for (let i = 0; i < brackets.length && left > 0; i++) {
-        const amount = left > brackets[i][0] - prev ? brackets[i][0] - prev : left;
-        const tax = amount * (brackets[i][1] / 100);
-        
-        left -= amount;
-        ans += tax;
-        prev = brackets[i][0];
-    }
-    
-    return ans;
+    return brackets.reduce(([tax, prev], [amt, percent]) => {
+        if (income <= 0) brackets.length = 0;
+        const current = income > amt - prev ? amt - prev : income;
+        tax += current * (percent / 100);
+        income -= current;
+        return [tax, amt];
+    },[0, 0])[0];
 };
