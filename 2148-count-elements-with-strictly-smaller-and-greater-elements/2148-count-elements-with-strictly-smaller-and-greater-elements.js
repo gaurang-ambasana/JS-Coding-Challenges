8c707bc84d858f3d05886beddc454220fdc9f85d
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-21 00:14:32 +0530

    Time: 66 ms (91.61%), Space: 42 MB (81.12%) - LeetHub

--- 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.js
+++ 2148-count-elements-with-strictly-smaller-and-greater-elements/2148-count-elements-with-strictly-smaller-and-greater-elements.js
@@ -2,9 +2,6 @@
  * @param {number[]} nums
  * @return {number}
  */
-const countElements = nums => {    
-    const max = Math.max(...nums);
-    const min = Math.min(...nums);
-    
-    return nums.reduce((count, ele) => ele !== max && ele !== min ? ++count : count, 0);
+const countElements = nums => {
+    return nums.sort((a, b) => b - a).reduce((count, ele, _, arr) => ele !== arr[0] && ele !== arr.at(-1) ? ++count : count, 0);
 }
