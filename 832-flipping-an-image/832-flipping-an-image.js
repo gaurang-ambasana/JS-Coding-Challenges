f3c1c1744b4b986b2c9e09d037bc2971e34b0a87
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-21 00:55:25 +0530

    Time: 83 ms (69.35%), Space: 43.8 MB (67.77%) - LeetHub

--- 832-flipping-an-image/832-flipping-an-image.js
+++ 832-flipping-an-image/832-flipping-an-image.js
@@ -2,4 +2,4 @@
  * @param {number[][]} image
  * @return {number[][]}
  */
-const flipAndInvertImage = image => image.map(row => row.reverse()).map(row => row.map(e => e === 0 ? 'o' : 'z')).map(row => row.map(e => e === 'o' ? 1 : 0));
+const flipAndInvertImage = image => image.map(row => row.reverse().map(e => e ^ 1));
