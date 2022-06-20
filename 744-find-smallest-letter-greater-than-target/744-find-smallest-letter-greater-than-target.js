f29430c75691272e57fa1ba4c258eda163dac0a2
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-21 00:42:27 +0530

    Time: 86 ms (57.01%), Space: 43.9 MB (36.11%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.js
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.js
@@ -6,5 +6,5 @@
 const nextGreatestLetter = (letters, target) => {
     const ans = letters.reduce((min, l) => l > target ? (min.length ? (l < min ? l : min) : l) : min, '');
     
-    return ans.length ? ans : letters[0];
+    return ans.length ? ans : letters.at(0);
 };
