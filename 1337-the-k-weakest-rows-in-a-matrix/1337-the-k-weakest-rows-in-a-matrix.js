8a25ea0348b33ff727e108cfe74a4856c2df1cfb
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-21 13:11:23 +0530

    Time: 104 ms (32.77%), Space: 44.9 MB (18.28%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.js
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.js
@@ -3,4 +3,4 @@
  * @param {number} k
  * @return {number[]}
  */
-const kWeakestRows = (mat, k) => mat.map((row, i) => {return {1: row.filter(Boolean).length, i}}).sort((a, b) => a[1] - b[1]).slice(0, k).map(({i}) => i);
+const kWeakestRows = (mat, k) => mat.map((row, i) => [ row.filter(Boolean).length, i]).sort((a, b) => a[0] - b[0]).slice(0, k).map(([_, i]) => i);
