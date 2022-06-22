ac17b485ec6ef9e680c0a7411d1299e7142ea88e
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-22 12:39:39 +0530

    Time: 133 ms (32.04%), Space: 44.6 MB (86.46%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+const topKFrequent = function(nums, k) {
+    const frequency = nums.reduce((counter, n) => (counter[n] = (counter[n] ?? 0) + 1, counter), {});
+    return [...new Set(nums)].sort((a, b) => frequency[b] - frequency[a]).slice(0, k);    
+};
