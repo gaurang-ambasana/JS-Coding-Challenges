74d77c052907537e514c6e70888570ead8221fe4
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-22 18:05:15 +0530

    Time: 323 ms (27.01%), Space: 71.3 MB (12.22%) - LeetHub

--- 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.js
+++ 2284-sender-with-largest-word-count/2284-sender-with-largest-word-count.js
@@ -3,4 +3,4 @@
  * @param {string[]} senders
  * @return {string}
  */
-var largestWordCount = (messages, senders) => Object.entries(senders.reduce((counter, name, i) => (counter[name] = (counter[name] ?? 0) + messages[i].split(/\s/).length, counter), {})).sort((a, b) => b[1] - a[1] !== 0 ? b[1] - a[1] : (a[0] > b[0] ? -1 : 1))[0][0];
+const largestWordCount = (messages, senders) => Object.entries(senders.reduce((counter, name, i) => (counter[name] = (counter[name] ?? 0) + messages[i].split(/\s/).length, counter), {})).sort((a, b) => b[1] - a[1] !== 0 ? b[1] - a[1] : (a[0] > b[0] ? -1 : 1))[0][0];
