124f68b0784abcd122edfcff42b47491a2cdfca3
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-22 12:29:47 +0530

    Time: 84 ms (66.00%), Space: 44 MB (64.77%) - LeetHub

--- 414-third-maximum-number/414-third-maximum-number.js
+++ 414-third-maximum-number/414-third-maximum-number.js
@@ -2,6 +2,4 @@
  * @param {number[]} nums
  * @return {number}
  */
-var thirdMax = function(nums) {
-    return new Set(nums).size >= 3 ? [...new Set(nums)].sort((a, b) => b - a)[2] : [...new Set(nums)].reduce((max, e) => max > e ? max : e, -Infinity);
-};
+const thirdMax = (nums) => new Set(nums).size >= 3 ? [...new Set(nums)].sort((a, b) => b - a)[2] : [...new Set(nums)].reduce((max, e) => max > e ? max : e, -Infinity);
