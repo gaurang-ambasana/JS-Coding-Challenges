c99b5e781e3cd07a0337558ef2ded70db511b9f6
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-22 18:57:13 +0530

    Time: 81 ms (81.51%), Space: 44.2 MB (87.67%) - LeetHub

--- 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.js
+++ 1380-lucky-numbers-in-a-matrix/1380-lucky-numbers-in-a-matrix.js
@@ -2,8 +2,9 @@
  * @param {number[][]} matrix
  * @return {number[]}
  */
-var luckyNumbers  = function(matrix) {
+const luckyNumbers = (matrix) => {
     const minRow = matrix.map(row => Math.min(...row));
+    
     return matrix[0].reduce((arr, _, i) => {
         const col = matrix.map(r => r[i]);
         const max = Math.max(...col);
