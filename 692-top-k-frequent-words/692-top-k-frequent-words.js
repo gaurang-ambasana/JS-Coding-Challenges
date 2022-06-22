870598370c6a6e69056f3faf93fbe814fa778dcc
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-22 12:54:41 +0530

    Time: 121 ms (44.18%), Space: 47.4 MB (38.36%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.js
+++ 692-top-k-frequent-words/692-top-k-frequent-words.js
@@ -3,4 +3,4 @@
  * @param {number} k
  * @return {string[]}
  */
-const topKFrequent = (words, k) => Object.entries(words.reduce((counter, word) => (counter[word] = (counter[word] ?? 0) + 1, counter), {})).sort((a, b) => b[1] - a[1] === 0 ? a[0].localeCompare(b[0]) : b[1] - a[1]).slice(0, k).map(([word, _]) => word);
+const topKFrequent = (words, k) => Object.entries(words.reduce((c, w) => (c[w] = (c[w] ?? 0) + 1, c), {})).sort((a, b) => b[1] - a[1] === 0 ? a[0].localeCompare(b[0]) : b[1] - a[1]).slice(0, k).map(([w, _]) => w);
