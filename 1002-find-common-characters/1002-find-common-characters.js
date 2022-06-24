0da4245d241bc70038bef752957f5b051b2dcabe
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-24 13:55:02 +0530

    Time: 126 ms (41.64%), Space: 48.4 MB (31.23%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.js
+++ 1002-find-common-characters/1002-find-common-characters.js
@@ -2,16 +2,15 @@
  * @param {string[]} words
  * @return {string[]}
  */
-var commonChars = function(words) {
-    return words.sort((a, b) => b.length - a.length).at(0).split('').reduce((arr, c) => {
-        let flag = true;
+const commonChars = (words) => words.sort((a, b) => b.length - a.length).at(0).split('').reduce((arr, c) => {
+    let flag = true;
         
-        for (const i in words) {
-            if (!words[i].includes(c)) flag = false;
-            words[i] = words[i].replace(c, '');
-        }
+    for (const i in words) {
+        if (!words[i].includes(c)) flag = false;
+        words[i] = words[i].replace(c, '');
+    }
         
-        flag && arr.push(c);
-        return arr;
-    }, []);
-};
+    flag && arr.push(c);
+        
+    return arr;
+}, []);
