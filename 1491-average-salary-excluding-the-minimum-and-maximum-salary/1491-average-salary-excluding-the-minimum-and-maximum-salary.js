13844bf067113be4e9ddcb200b5d894d43f976ab
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-26 21:21:17 +0530

    Time: 88 ms (41.04%), Space: 41.8 MB (63.87%) - LeetHub

---
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} salary
+ * @return {number}
+ */
+var average = function(salary) {
+    const max = Math.max(...salary);
+    const min = Math.min(...salary);
+    salary.splice(salary.indexOf(max), 1);
+    salary.splice(salary.indexOf(min), 1);
+    return salary.reduce((sum, e) => sum + e, 0) / salary.length;
+};
