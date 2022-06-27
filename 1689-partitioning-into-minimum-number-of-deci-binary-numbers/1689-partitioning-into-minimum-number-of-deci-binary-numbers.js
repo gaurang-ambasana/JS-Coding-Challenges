8169bf54081b298bc7bbee7d2841902b233faa94
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-27 12:40:04 +0530

    Time: 170 ms (31.60%), Space: 51.1 MB (42.35%) - LeetHub

--- 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.js
+++ 1689-partitioning-into-minimum-number-of-deci-binary-numbers/1689-partitioning-into-minimum-number-of-deci-binary-numbers.js
@@ -2,4 +2,4 @@
  * @param {string} n
  * @return {number}
  */
-const minPartitions = n => n.split(``).map(Number).reduce((max, num) => max > num ? max : num, 0);
+const minPartitions = (n) => [...n].map(Number).reduce((max, num) => max > num ? max : num, 0);
