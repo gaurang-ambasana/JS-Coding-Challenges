8302d5b632f92381a4fe7b03c899847717e08d5e
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-29 15:51:53 +0530

    Time: 101 ms (90.42%), Space: 51.6 MB (65.50%) - LeetHub

--- 189-rotate-array/189-rotate-array.js
+++ 189-rotate-array/189-rotate-array.js
@@ -3,4 +3,4 @@
  * @param {number} k
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-const rotate = (nums, k) => nums.splice(0, 0, ...nums.splice(nums.length - (k % nums.length), (k % nums.length)));
+const rotate = (nums, k) => k === 0 ? nums : nums.splice(0, 0, ...nums.splice(nums.length - (k % nums.length), (k % nums.length)));
