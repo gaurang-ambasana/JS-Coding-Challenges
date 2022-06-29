40203df592bb60dfb03ed8fc6f804f5c3d000cad
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-29 10:44:48 +0530

    Time: 206 ms (6.47%), Space: 46 MB (14.94%) - LeetHub

--- 976-largest-perimeter-triangle/976-largest-perimeter-triangle.js
+++ 976-largest-perimeter-triangle/976-largest-perimeter-triangle.js
@@ -3,7 +3,7 @@
  * @return {number}
  */
 var largestPerimeter = function(nums) {
-    nums.sort((a, b) => a - b);
+    nums.sort((a,b) => a - b);
     
     for (let i = nums.length - 1; i >= 2; i--) {
         const a = nums[i];
