5007f51ed7425649a42a43d863f274b646b1e431
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-30 00:45:22 +0530

    Time: 164 ms (23.39%), Space: 51.1 MB (19.31%) - LeetHub

--- 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.js
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate/1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.js
@@ -7,6 +7,6 @@
 const nearestValidPoint = (x, y, points) => {
     const distanceAtIdx = points.reduce((o, [a, b], i) => x === a || y === b ? (o[i] = Math.abs(x - a) + Math.abs(y - b), o) : o, {});
     const min = Math.min(...Object.values(distanceAtIdx));
-    const ans = Object.keys(distanceAtIdx).filter((idx) => distanceAtIdx[idx] === min);
+    const ans = Object.keys(distanceAtIdx).filter((i) => distanceAtIdx[i] === min);
     return ans.length === 0 ? -1 : Math.min(...ans);
 };
