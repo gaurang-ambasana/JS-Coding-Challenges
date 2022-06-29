0e8ef7ffbebf01ba3e0106c89b76be928fc23a6c
Gaurang Ambasana <gpambasana@gmail.com>
2022-06-30 00:59:23 +0530

    Time: 105 ms (21.87%), Space: 43.5 MB (58.12%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.js
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.js
@@ -2,7 +2,4 @@
  * @param {number[]} nums
  * @return {number}
  */
-const arraySign = nums => {
-    const product = nums.reduce((p, n) => p * n, 1);
-    return product > 0 ? 1 : product < 0 ? -1 : 0;
-}
+const arraySign = (nums) => nums.includes(0) ? 0 : Math.sign(nums.reduce((p, n) => p * n, 1));
