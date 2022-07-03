1ebbdd4017f35ba0747a4d591e0975e4d282c95f
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-03 15:11:53 +0530

    Time: 124 ms (12.11%), Space: 43.4 MB (15.22%) - LeetHub

---
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var canMakeArithmeticProgression = function(arr) {
+    if (arr.every(v => v === arr[0]))
+        return true;
+    
+    if (new Set(arr).size !== arr.length)
+        return false;
+    
+    arr.sort((a, b) => a - b);
+    
+    let a = arr[0];
+    const d = arr[1] - arr[0];
+    
+    for (let i = 0, n = arr.length - 1; i < n; i++) {
+        const nxt = a + d;
+        
+        if (!arr.includes(nxt))
+            return false;
+        
+        a = nxt;
+    }
+    
+    return true;
+};
