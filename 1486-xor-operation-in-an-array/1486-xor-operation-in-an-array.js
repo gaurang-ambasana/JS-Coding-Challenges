56a566521dd8b7e88824d80f15bb2472638f4108
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-04 19:54:01 +0530

    Time: 86 ms (48.86%), Space: 42.1 MB (35.62%) - LeetHub

---
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @param {number} start
+ * @return {number}
+ */
+var xorOperation = function(n, start) {
+    const arr = [];
+    let i = 1;
+    
+    while (arr.length !== n - 1) 
+        arr.push(start + 2 * i++);
+    
+    return arr.reduce((r, e) => r ^ e, start);
+};
