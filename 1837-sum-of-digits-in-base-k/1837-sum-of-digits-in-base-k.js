6a4b3fdfb22574053fda004c0924687507b69c62
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-04 20:10:50 +0530

    Time: 70 ms (72.38%), Space: 41.9 MB (61.90%) - LeetHub

--- 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.js
+++ 1837-sum-of-digits-in-base-k/1837-sum-of-digits-in-base-k.js
@@ -3,4 +3,4 @@
  * @param {number} k
  * @return {number}
  */
-var sumBase = (n, k) => n.toString(k).split(``).reduce((res, digit) => Number(res) + Number(digit), 0);
+var sumBase = (n, k) => n.toString(k).split('').reduce((res, digit) => Number(res) + Number(digit), 0);
