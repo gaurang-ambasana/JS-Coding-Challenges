7d89555d03125cd09be5a8cc4ef441906deed667
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-05 18:00:38 +0530

    Time: 182 ms (10.00%), Space: 52.6 MB (90.00%) - LeetHub

--- 1835-find-xor-sum-of-all-pairs-bitwise-and/1835-find-xor-sum-of-all-pairs-bitwise-and.js
+++ 1835-find-xor-sum-of-all-pairs-bitwise-and/1835-find-xor-sum-of-all-pairs-bitwise-and.js
@@ -6,4 +6,4 @@
 
 // Since (X & A) ^ (X & B) = X & (A ^ B) 
 
-const getXORSum = (arr1, arr2) => arr1.reduce((a, b) => a ^ b) & arr2.reduce((a, b) => a ^ b);
+var getXORSum = (arr1, arr2) => arr1.reduce((a, b) => a ^ b) & arr2.reduce((a, b) => a ^ b);
