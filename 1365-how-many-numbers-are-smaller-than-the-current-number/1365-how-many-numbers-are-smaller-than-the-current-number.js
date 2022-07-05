18326dd95b5c07f2f9bd626984535bd82a65b8e2
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-05 19:22:57 +0530

    Time: 124 ms (41.72%), Space: 43.1 MB (99.93%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
@@ -2,4 +2,4 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-const smallerNumbersThanCurrent = nums => nums.map(n => nums.reduce((c, e) => e < n ? ++c : c, 0));
+var smallerNumbersThanCurrent = nums => nums.map(n => nums.reduce((c, e) => e < n ? ++c : c, 0));
