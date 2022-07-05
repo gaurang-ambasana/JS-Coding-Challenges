4b53c8bf0628e21e2bff2fa9026ce3277aa54720
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-05 18:46:50 +0530

    Time: 121 ms (66.67%), Space: 42.1 MB (81.48%) - LeetHub

--- 1995-count-special-quadruplets/1995-count-special-quadruplets.js
+++ 1995-count-special-quadruplets/1995-count-special-quadruplets.js
@@ -2,7 +2,7 @@
  * @param {number[]} nums
  * @return {number}
  */
-const countQuadruplets = (nums) => {
+const countQuadruplets = nums => {
     let res = 0;
     
     for (let i = 0; i < nums.length; i++)
