d79b78aeecafdabcdaf58ff4af38b6002da03074
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-06 23:12:35 +0530

    Time: 278 ms (6.43%), Space: 49.6 MB (62.95%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.js
+++ 1603-design-parking-system/1603-design-parking-system.js
@@ -1,35 +1,22 @@
-/**
- * @param {number} big
- * @param {number} medium
- * @param {number} small
- */
-var ParkingSystem = function(big, medium, small) {
-    this.parking = {
-        big,
-        medium,
-        small,
-    };
-};
-
-/** 
- * @param {number} carType
- * @return {boolean}
- */
-ParkingSystem.prototype.addCar = function(carType) {
-    if (carType === 1) {
-        this.parking.big--;
-        return this.parking.big >= 0;
-    } else if (carType === 2) {
-        this.parking.medium--;
-        return this.parking.medium >= 0;
-    } else if (carType === 3) {
-        this.parking.small--;
-        return this.parking.small >= 0;
+class ParkingSystem {
+    constructor (big, medium, small) {
+        this.parking = {
+            big,
+            medium,
+            small,
+        };
+    }
+    
+    addCar (carType) {
+        if (carType === 1) {
+            this.parking.big--;
+            return this.parking.big >= 0;
+        } else if (carType === 2) {
+            this.parking.medium--;
+            return this.parking.medium >= 0;
+        } else if (carType === 3) {
+            this.parking.small--;
+            return this.parking.small >= 0;
+        }
     }
 };
-
-/** 
- * Your ParkingSystem object will be instantiated and called as such:
- * var obj = new ParkingSystem(big, medium, small)
- * var param_1 = obj.addCar(carType)
- */
