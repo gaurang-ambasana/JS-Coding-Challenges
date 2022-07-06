8270cbb54ae90c6c67f9b58c354f7cda095106a0
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-06 12:23:20 +0530

    Time: 75 ms (78.85%), Space: 41.8 MB (59.33%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    const s = [0, 1];
+    
+    for (let i = 2; i < n + 1; i++)
+        s.push(s[i - 1] + s[i - 2]);
+    
+    return s[n];
+};
