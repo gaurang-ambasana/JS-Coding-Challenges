aba94be7ee6a29b3651f4a62cd554cbb339d6526
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-06 19:28:55 +0530

    Time: 285 ms (16.21%), Space: 48.6 MB (22.63%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.js
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.js
@@ -3,6 +3,6 @@
  * @return {number[]}
  */
 var sortByBits = (arr) => arr.sort((a, b) => {
-    const d = a.toString(2).split(``).reduce((c, a) => a === '1' ? ++c : c, 0) - b.toString(2).split(``).reduce((c, a) => a === '1' ? ++c : c, 0);
+    const d = a.toString(2).replaceAll(`0`,``).length - b.toString(2).replaceAll(`0`,``).length;
     return d ? d : a - b;
 });
