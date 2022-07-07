105a1681a66b49b81499c91924dd75658f374798
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-08 01:02:27 +0530

    Time: 166 ms (9.61%), Space: 47.4 MB (11.54%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.js
+++ 1402-reducing-dishes/1402-reducing-dishes.js
@@ -2,4 +2,7 @@
  * @param {number[]} satisfaction
  * @return {number}
  */
-const maxSatisfaction = satisfaction => satisfaction.sort((a, b) => a - b).map((_, i, arr) => arr.slice(i).reduce((a, e, idx) => a + (e * (idx + 1)), 0)).reduce((max, e) => max > e ? max : e, 0);
+const maxSatisfaction = satisfaction => satisfaction
+    .sort((a, b) => a - b)
+    .map((_, i, arr) => arr.slice(i).reduce((a, e, idx) => a + (e * (idx + 1)), 0))
+    .reduce((max, e) => max > e ? max : e, 0);
