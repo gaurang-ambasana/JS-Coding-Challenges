9898ae4832a5c5813bb5c37f16d3740f5a70bf80
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-07 17:07:56 +0530

    Time: 78 ms (76.19%), Space: 43.6 MB (88.78%) - LeetHub

--- 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.js
+++ 1232-check-if-it-is-a-straight-line/1232-check-if-it-is-a-straight-line.js
@@ -1,4 +1,4 @@
-const checkStraightLine = (coordinates) => {
+const checkStraightLine = coordinates => {
   const isStraight = (x, y, z) => (y[1] - x[1]) * (z[0] - y[0]) === (y[0] - x[0]) * (z[1] - y[1]);
     
     for (let i = 1, n = coordinates.length - 1; i < n; i++)
