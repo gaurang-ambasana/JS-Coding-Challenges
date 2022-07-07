20725146bdfbb581eb97a3e5ecfd04b0d1e49738
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-08 00:39:59 +0530

    Time: 82 ms (76.71%), Space: 42.3 MB (57.34%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.js
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.js
@@ -2,4 +2,4 @@
  * @param {number[]} arr
  * @return {number}
  */
-const sumOddLengthSubarrays = arr => arr.reduce((s, e, i) => s + Math.ceil((i + 1) * (arr.length - i) / 2) * e, 0);
+const sumOddLengthSubarrays = (arr) => arr.reduce((s, e, i) => s + Math.ceil((i + 1) * (arr.length - i) / 2) * e, 0);
