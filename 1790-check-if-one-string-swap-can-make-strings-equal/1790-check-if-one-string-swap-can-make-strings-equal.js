da093e78f797f64da6b97522161fcee2995ef246
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-08 00:22:50 +0530

    Time: 127 ms (11.68%), Space: 43.4 MB (21.65%) - LeetHub

---
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var areAlmostEqual = function(s1, s2) {
+    let diff = '';    
+    
+    for (const i in s1)
+        if (s1[i] !== s2[i])
+            diff += s1[i] + s2[i];
+    
+    return diff.length < 5 && diff[0] === diff[3] && diff[1] === diff[2];
+};
