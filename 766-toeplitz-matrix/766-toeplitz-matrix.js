ab58e7be62eeb8596e23bc6590604d56e604963b
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-09 21:17:10 +0530

    Time: 148 ms (10.26%), Space: 44.4 MB (21.65%) - LeetHub

--- 766-toeplitz-matrix/766-toeplitz-matrix.js
+++ 766-toeplitz-matrix/766-toeplitz-matrix.js
@@ -6,10 +6,9 @@ const isToeplitzMatrix = matrix => {
     let start = 0;
     
     while (start < matrix.length) { 
-        for (let i = 0; i < matrix[0].length; i++) {
+        for (let i = 0; i < matrix[0].length; i++)
             if (start !== matrix.length - 1 && i !== matrix[0].length - 1 && matrix[start][i] !== matrix[start + 1][i + 1])
                 return false;
-        }
         
         start++;
     }
