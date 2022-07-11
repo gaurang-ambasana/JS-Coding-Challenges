4cbc56b0a371b6f22abd473da431513a2e859cdb
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-11 18:44:46 +0530

    Time: 78 ms (83.55%), Space: 44.2 MB (76.82%) - LeetHub

--- 1528-shuffle-string/1528-shuffle-string.js
+++ 1528-shuffle-string/1528-shuffle-string.js
@@ -3,6 +3,4 @@
  * @param {number[]} indices
  * @return {string}
  */
-var restoreString = function(s, indices) {
-    return Object.values(indices.reduce((o, n, i) => (o[n] = s[i], o), {})).join(``);
-};
+const restoreString = (s, indices) => Object.values(indices.reduce((o, n, i) => (o[n] = s[i], o), {})).join(``);
