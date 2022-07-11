b354530dab5acc3a6b91d3ad72415a95b177d484
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-11 19:10:25 +0530

    Time: 118 ms (10.36%), Space: 41.8 MB (79.64%) - LeetHub

--- 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.js
+++ 1389-create-target-array-in-the-given-order/1389-create-target-array-in-the-given-order.js
@@ -6,7 +6,7 @@
 const createTargetArray = (nums, index) => {
     const target = [];
     
-    for (let i = 0, n = nums.length; i < n; i++)
+    for (let i = 0, n = index.length; i < n; i++)
         target.splice(index[i], 0, nums[i]);
     
     return target;
