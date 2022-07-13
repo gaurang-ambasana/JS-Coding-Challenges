d605ac2ab1aaf2cfe05f40ed2e07bfbc20ddcb4d
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-13 15:38:14 +0530

    Time: 142 ms (8.83%), Space: 43.9 MB (86.44%) - LeetHub

---
+++ 819-most-common-word/819-most-common-word.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string} paragraph
+ * @param {string[]} banned
+ * @return {string}
+ */
+var mostCommonWord = function(paragraph, banned) {
+    return Object.entries(paragraph.toLowerCase().replaceAll(/[^a-zA-Z ]/g, ` `).split(/\s+/).filter(Boolean).reduce((c, w) => (c[w] = (c[w] ?? 0) + 1, c), {})).sort(([, f1], [, f2]) => f2 - f1).find(([s,]) => !banned.includes(s))[0];
+};
