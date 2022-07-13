75ce9eee67da22e164c95ae6ccaf2c640677e2f7
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-13 16:23:41 +0530

    Time: 125 ms (56.28%), Space: 47.6 MB (44.13%) - LeetHub

--- 811-subdomain-visit-count/811-subdomain-visit-count.js
+++ 811-subdomain-visit-count/811-subdomain-visit-count.js
@@ -2,21 +2,17 @@
  * @param {string[]} cpdomains
  * @return {string[]}
  */
-var subdomainVisits = function(cpdomains) {
-    const count = {};
-    
-    for (let i = 0, n = cpdomains.length; i < n; i++) {
-        const [c, domain] = cpdomains[i].split(` `);
-        count[domain] = parseInt(count[domain] ?? 0) + parseInt(c);
+var subdomainVisits = (cpdomains) => Object.entries(cpdomains.reduce((count, url) => {
+    const [c, domain] = url.split(` `);
+    count[domain] = parseInt(count[domain] ?? 0) + parseInt(c);
         
-        const tld = domain.match(/(\.\w+)$/g).join(``).slice(1);
-        count[tld] = parseInt(count[tld] ?? 0) + parseInt(c);
+    const tld = domain.match(/(\.\w+)$/g).join(``).slice(1);
+    count[tld] = parseInt(count[tld] ?? 0) + parseInt(c);
         
-        if (domain.indexOf(`.`) !== domain.lastIndexOf(`.`)) {
-            const sld = domain.match(/(\w+\.\w+)$/g).join(``);
-            count[sld] = parseInt(count[sld] ?? 0) + parseInt(c);
-        }
+    if (domain.indexOf(`.`) !== domain.lastIndexOf(`.`)) {
+        const sld = domain.match(/(\w+\.\w+)$/g).join(``);
+        count[sld] = parseInt(count[sld] ?? 0) + parseInt(c);
     }
     
-    return Object.entries(count).map(([domain, visit]) => `${visit} ${domain}`);
-};
+    return count;
+}, {})).map(([domain, visit]) => `${visit} ${domain}`);
