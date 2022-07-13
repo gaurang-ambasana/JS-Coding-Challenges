4a7a88a8272e13088dc25cb2a06f9acff59e8f35
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-13 15:40:18 +0530

    Time: 86 ms (74.45%), Space: 44.2 MB (69.09%) - LeetHub

--- 819-most-common-word/819-most-common-word.js
+++ 819-most-common-word/819-most-common-word.js
@@ -3,4 +3,4 @@
  * @param {string[]} banned
  * @return {string}
  */
-const mostCommonWord = (paragraph, banned) => Object.entries(paragraph.toLowerCase().replaceAll(/[^a-zA-Z ]/g, ` `).split(/\s+/).filter(Boolean).reduce((c, w) => (c[w] = (c[w] ?? 0) + 1, c), {})).sort(([, f1], [, f2]) => f2 - f1).find(([s,]) => !banned.includes(s))[0];
+const mostCommonWord = (paragraph, banned) => Object.entries(paragraph.toLowerCase().replaceAll(/[^a-zA-Z ]/g, ` `).split(/\s+/).filter(w => w).reduce((c, w) => (c[w] = (c[w] ?? 0) + 1, c), {})).sort(([, f1], [, f2]) => f2 - f1).find(([w,]) => !banned.includes(w))[0];
