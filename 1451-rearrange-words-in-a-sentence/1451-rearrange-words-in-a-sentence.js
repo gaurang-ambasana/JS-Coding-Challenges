3f6bf3132ad42df2eee86b23c5dc7812286f65b0
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-13 15:15:26 +0530

    Time: 129 ms (30.88%), Space: 46.4 MB (52.94%) - LeetHub

--- 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.js
+++ 1451-rearrange-words-in-a-sentence/1451-rearrange-words-in-a-sentence.js
@@ -2,6 +2,4 @@
  * @param {string} text
  * @return {string}
  */
-var arrangeWords = function(text) {
-    return text.toLowerCase().split(` `).sort((a, b) => a.length - b.length).map((s, i) => i !== 0 ? s : s.charAt(0).toUpperCase() + s.slice(1)).join(` `); 
-};
+const arrangeWords = (text) => text.toLowerCase().split(` `).sort((a, b) => a.length - b.length).map((s, i) => i !== 0 ? s : s[0].toUpperCase() + s.slice(1)).join(` `);
