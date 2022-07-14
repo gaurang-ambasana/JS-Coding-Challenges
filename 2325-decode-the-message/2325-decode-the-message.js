1153a26de1b977914946f955bd6bba505ddb5ce9
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-14 17:31:31 +0530

    Time: 53 ms (99.74%), Space: 44.4 MB (82.38%) - LeetHub

--- 2325-decode-the-message/2325-decode-the-message.js
+++ 2325-decode-the-message/2325-decode-the-message.js
@@ -7,5 +7,5 @@ const decodeMessage = (key, message) => {
     const letters = `abcdefghijklmnopqrstuvwxyz`;
     const modKey = [...new Set(key.replaceAll(` `, ``).split(``))];
     const map = modKey.reduce((m, l, i) => (m[l] = letters[i], m), {});
-    return message.split(` `).map(w => w.split(``).map(c => map[c]).join(``)).join(` `);
+    return message.split(``).map(w => w in map ? map[w] : w).join(``);
 };
