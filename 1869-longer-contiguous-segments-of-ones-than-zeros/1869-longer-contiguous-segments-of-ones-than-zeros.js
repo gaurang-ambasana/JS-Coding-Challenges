641da9578ba06d24e4f8ac6cd442653a09740f2d
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-14 15:29:52 +0530

    Time: 102 ms (32.28%), Space: 43.5 MB (10.24%) - LeetHub

--- 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.js
+++ 1869-longer-contiguous-segments-of-ones-than-zeros/1869-longer-contiguous-segments-of-ones-than-zeros.js
@@ -2,9 +2,4 @@
  * @param {string} s
  * @return {boolean}
  */
-var checkZeroOnes = function(s) {
-    const ones = s.replaceAll('0', ',').split(',').filter(({ length }) => length > 0).reduce((max, { length }) => max > length ? max : length, 0);
-    const zeroes = s.replaceAll('1', ',').split(',').filter(({ length }) => length > 0).reduce((max, { length }) => max > length ? max : length, 0);
-    
-    return ones > zeroes;
-};
+const checkZeroOnes = (s) => s.replaceAll('0', ',').split(',').filter(({ length }) => length > 0).reduce((max, { length }) => max > length ? max : length, 0) > s.replaceAll('1', ',').split(',').filter(({ length }) => length > 0).reduce((max, { length }) => max > length ? max : length, 0);
