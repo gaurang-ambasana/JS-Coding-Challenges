e34eceed36bf1f7ea3e1f1f928186d124a76f365
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-14 17:39:29 +0530

    Time: 129 ms (5.39%), Space: 43.9 MB (5.15%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.js
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.js
@@ -2,4 +2,4 @@
  * @param {number[]} nums
  * @return {number}
  */
-var numIdenticalPairs = (nums) => nums.reduce((c, e, i) => nums.slice(i + 1).reduce((s, n) => n === e ? ++s : s, c), 0);
+const numIdenticalPairs = nums => nums.reduce((c, e, i) => nums.slice(i + 1).reduce((s, n) => e === n ? ++s : s, c), 0);
