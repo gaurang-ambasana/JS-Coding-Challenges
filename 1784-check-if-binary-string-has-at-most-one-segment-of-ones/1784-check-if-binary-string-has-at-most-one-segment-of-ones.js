521f6ee3d3f6ecb836c7d7a266b5927563888b22
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-14 16:13:06 +0530

    Time: 65 ms (92.31%), Space: 41.6 MB (92.31%) - LeetHub

--- 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.js
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones/1784-check-if-binary-string-has-at-most-one-segment-of-ones.js
@@ -2,4 +2,4 @@
  * @param {string} s
  * @return {boolean}
  */
-var checkOnesSegment = (s) => s.split('0').filter(Boolean).length < 2;
+var checkOnesSegment = s => s.split(`0`).filter(Boolean).length < 2;
