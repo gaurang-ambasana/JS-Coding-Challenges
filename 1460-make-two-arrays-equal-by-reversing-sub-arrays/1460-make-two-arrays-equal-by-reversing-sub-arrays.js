fd314a0ccfd832d359674855d60729cf8255032c
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-16 17:49:13 +0530

    Time: 113 ms (38.56%), Space: 45.6 MB (16.95%) - LeetHub

--- 1460-make-two-arrays-equal-by-reversing-sub-arrays/1460-make-two-arrays-equal-by-reversing-sub-arrays.js
+++ 1460-make-two-arrays-equal-by-reversing-sub-arrays/1460-make-two-arrays-equal-by-reversing-sub-arrays.js
@@ -3,9 +3,11 @@
  * @param {number[]} arr
  * @return {boolean}
  */
-var canBeEqual = function(target, arr) {
+var canBeEqual = (target, arr) => {
     const getFreq = array => array.reduce((c, e) => (c[e] = (c[e] ?? 0) + 1, c), {});
+    
     const targetCounter = getFreq(target);
     const arrCounter = getFreq(arr);
+    
     return Object.keys(targetCounter).every(e => (targetCounter[e] === arrCounter[e]));
 };
