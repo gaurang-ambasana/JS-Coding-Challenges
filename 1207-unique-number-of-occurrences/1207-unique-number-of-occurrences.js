30c5b43add6c3c7684c6a91dbd6c723ce0627f59
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-20 00:37:52 +0530

    Time: 63 ms (94.33%), Space: 43 MB (38.08%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.js
@@ -2,4 +2,4 @@
  * @param {number[]} arr
  * @return {boolean}
  */
-var uniqueOccurrences = (arr) => new Set(Object.values(arr.reduce((c, n) => (c[n] = (c[n] || 0) + 1, c), {}))).size === new Set(arr).size;
+var uniqueOccurrences = (arr) => new Set(Object.values(arr.reduce((c, n) => (c[n] = (c[n] ?? 0) + 1, c), {}))).size === new Set(arr).size;
