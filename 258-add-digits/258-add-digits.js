24434982d575a3bad70f32f8b3bebed5aa6750d7
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-19 11:40:18 +0530

    Time: 98 ms (65.52%), Space: 44.4 MB (16.11%) - LeetHub

--- 258-add-digits/258-add-digits.js
+++ 258-add-digits/258-add-digits.js
@@ -1,6 +1,7 @@
 const addDigits = num => {
-    while (num.toString().length > 1)
+    do {
         num = num.toString().split(``).reduce((sum, n) => parseInt(sum) + parseInt(n), 0);
+    } while (num.toString().length > 1)
     
     return num;
 }
