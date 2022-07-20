1960d106403c5c84a3622c44e6c230d10b9e1caf
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-20 22:51:10 +0530

    Time: 342 ms (37.50%), Space: 72 MB (12.50%) - LeetHub

--- 2227-encrypt-and-decrypt-strings/2227-encrypt-and-decrypt-strings.js
+++ 2227-encrypt-and-decrypt-strings/2227-encrypt-and-decrypt-strings.js
@@ -1,15 +1,14 @@
 class Encrypter {
     constructor (keys, values, dictionary) {
         this.encryptMap = keys.reduce((map, key, i) => (map[key] = values[i], map), {});
-        
         this.encryptedVals = [...new Set(dictionary)].map(w => this.encrypt(w));
     }
     
-    encrypt (word1) {
-        return word1.split(``).reduce((s, c) => s + this.encryptMap[c], ``);
+    encrypt (word) {
+        return word.split(``).reduce((s, c) => s + this.encryptMap[c], ``);
     }
     
-    decrypt (word2) {
-        return this.encryptedVals.filter(x => x === word2).length;
+    decrypt (word) {
+        return this.encryptedVals.filter(x => x === word).length;
     }
 }
