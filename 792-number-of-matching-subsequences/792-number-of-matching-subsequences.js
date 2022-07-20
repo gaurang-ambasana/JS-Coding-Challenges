203cfbeca72a9d33e0564a4f431d3c33bc814b20
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-20 23:42:09 +0530

    Time: 211 ms (67.36%), Space: 51.1 MB (61.98%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.js
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.js
@@ -3,21 +3,21 @@
  * @param {string[]} words
  * @return {number}
  */
-var numMatchingSubseq = (s, words) => {
-    return words.reduce((res, w) => {
-        let flag = true;
-        let idx = 0;
-        for (let i = 0, n = w.length; i < n; i++) {
-            const x = s.slice(idx).indexOf(w[i]);
-            
-            if (x !== -1) idx += (x + 1);
-            else {
-                i = n;
-                flag = false;
-            }  
-        }
+const numMatchingSubseq = (s, words) => words.reduce((res, w) => {
+    let flag = true;
+    let idx = 0;
+    for (let i = 0, n = w.length; i < n; i++) {
+        const x = s.slice(idx).indexOf(w[i]);
         
-        flag && ++res;
-        return res;
-    }, 0);
-};
+        if (x !== -1)
+            idx += (x + 1);
+        else {
+            i = n;
+            flag = false;
+        }
+    }
+    
+    flag && ++res;
+    
+    return res;
+}, 0);
