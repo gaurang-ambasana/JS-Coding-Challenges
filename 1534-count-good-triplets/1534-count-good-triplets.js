6c5e34b7b20209f8cd0c29bd8e86e06972b4a1db
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-20 22:28:42 +0530

    Time: 123 ms (29.29%), Space: 42.5 MB (13.64%) - LeetHub

---
+++ 1534-count-good-triplets/1534-count-good-triplets.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} arr
+ * @param {number} a
+ * @param {number} b
+ * @param {number} c
+ * @return {number}
+ */
+const countGoodTriplets = function(arr, a, b, c) {
+    let ans = 0;
+    
+    for (let i = 0, n = arr.length; i < n; i++)
+        for (let j = i + 1; j < n; j++)
+            for (let k = j + 1; k < n; k++)
+                if (Math.abs(arr[i] - arr[j]) < a + 1)
+                    if (Math.abs(arr[j] - arr[k]) < b + 1)
+                        if (Math.abs(arr[i] - arr[k]) < c + 1)
+                            ans++;
+    
+    return ans;
+};
