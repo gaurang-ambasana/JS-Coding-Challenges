44aff6924bbde95b80cf536e203969a9a7794467
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-21 18:51:45 +0530

    Time: 83 ms (68.22%), Space: 43.5 MB (55.03%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.js
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.js
@@ -2,7 +2,7 @@
  * @param {number[]} arr
  * @return {boolean}
  */
-const checkIfExist = arr => arr.some((e, i) => {
+var checkIfExist = arr => arr.some((e, i) => {
     const x = [...arr];
     x.splice(i, 1);
     return x.includes(e * 2);
