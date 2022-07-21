3c4ff7dc3084ef0813161d1609ba2f95e20d309a
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-21 18:34:23 +0530

    Time: 102 ms (30.40%), Space: 41.5 MB (97.67%) - LeetHub

---
+++ 747-largest-number-at-least-twice-of-others/747-largest-number-at-least-twice-of-others.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var dominantIndex = function(nums) {
+    const max = nums.reduce((m, n) => m > n ? m : n, -1);
+    const idx = nums.indexOf(max);
+    nums.splice(idx, 1);
+    return nums.every(n => max >= n * 2) ? idx : -1;
+};
