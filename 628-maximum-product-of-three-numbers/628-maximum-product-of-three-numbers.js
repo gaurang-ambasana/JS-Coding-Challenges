a7820b7c53146c000bae5735aea3f65342a4b98e
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-22 13:36:54 +0530

    Time: 158 ms (50.26%), Space: 47.1 MB (24.49%) - LeetHub

---
+++ 628-maximum-product-of-three-numbers/628-maximum-product-of-three-numbers.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumProduct = nums => {
+    if (nums.length === 3) 
+        return nums.reduce((p, n) => p * n, 1);
+    else {
+        nums.sort((a, b) => b - a);
+        return Math.max(nums[0] * nums[1] * nums[2], nums.at(-1) * nums.at(-2) * nums[0]);
+    }
+}
