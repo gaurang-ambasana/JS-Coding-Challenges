bc45773314f08cb1d475e867b38fd198a3646d8f
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-23 15:58:28 +0530

    Time: 104 ms (16.86%), Space: 41.9 MB (67.68%) - LeetHub

---
+++ 500-keyboard-row/500-keyboard-row.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var findWords = (words) => words.filter(w => {
+    const wArr = w.toLowerCase().split(``);
+    return wArr.every(c => `qwertyuiop`.includes(c)) || wArr.every(c => `asdfghjkl`.includes(c)) || wArr.every(c => `zxcvbnm`.includes(c));
+});
