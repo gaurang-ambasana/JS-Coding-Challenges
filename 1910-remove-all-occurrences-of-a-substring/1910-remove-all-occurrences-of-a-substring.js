c84085762f7aa5278ca8b413d56e2d1048f055c3
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-23 16:13:40 +0530

    Time: 81 ms (67.89%), Space: 42.6 MB (49.54%) - LeetHub

---
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @param {string} part
+ * @return {string}
+ */
+var removeOccurrences = (s, part) => {
+    let len;
+    
+    while (len !== s.length) {
+        len = s.length;
+        s = s.replace(part, ``);
+    }
+    
+    return s;
+};
