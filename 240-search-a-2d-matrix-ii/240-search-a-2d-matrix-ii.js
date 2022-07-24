6b3ba61a72114b09eec0a639d2f84f644159174a
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-24 17:30:40 +0530

    Time: 938 ms (45.24%), Space: 45.4 MB (36.61%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.js
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.js
@@ -6,8 +6,9 @@
 var searchMatrix = (matrix, target) => {
     let row = 0;
     let col = matrix[row].length - 1;
+    const n = matrix.length;
     
-    while (row < matrix.length && col > -1)
+    while (row < n && col > -1)
         if (matrix[row][col] === target)
             return true;
         else if (matrix[row][col] > target)
