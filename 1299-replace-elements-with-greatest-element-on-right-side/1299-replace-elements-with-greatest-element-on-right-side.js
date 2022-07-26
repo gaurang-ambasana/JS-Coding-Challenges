ea4d2b98e076a68c51aadba53ca7d46f0e6bfde1
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-26 12:42:38 +0530

    Time: 1413 ms (8.63%), Space: 58.5 MB (5.19%) - LeetHub

--- 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.js
+++ 1299-replace-elements-with-greatest-element-on-right-side/1299-replace-elements-with-greatest-element-on-right-side.js
@@ -2,4 +2,4 @@
  * @param {number[]} arr
  * @return {number[]}
  */
-var replaceElements = arr => arr.slice(0, arr.length - 1).map((_, i) => arr.slice(i + 1).reduce((max, n) => max < n ? n : max, 0)).concat([-1]);
+var replaceElements = arr => arr.slice(0, arr.length - 1).map((_, i) => Math.max(...arr.slice(i + 1))).concat([-1]);
