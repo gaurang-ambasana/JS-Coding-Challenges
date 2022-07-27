163cf760a49bcec63d25674d60e1d9124ee88d77
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-27 17:45:34 +0530

    Time: 1026 ms (60.59%), Space: 43.6 MB (21.61%) - LeetHub

--- 220-contains-duplicate-iii/220-contains-duplicate-iii.js
+++ 220-contains-duplicate-iii/220-contains-duplicate-iii.js
@@ -6,8 +6,8 @@
  */
 var containsNearbyAlmostDuplicate = (nums, k, t) => {
     for (let i = 0, n = nums.length; i < n; i++)
-        for (let j = i + 1; j < n; j++)
-            if (Math.abs(i - j) < k + 1 && Math.abs(nums[i] - nums[j]) < t + 1)
+        for (let j = i + 1; j < i + k + 1; j++)
+            if (Math.abs(nums[i] - nums[j]) < t + 1)
                 return true;
     
     return false;
