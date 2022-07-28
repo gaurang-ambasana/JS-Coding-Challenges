4c608115d68e01cfa49a55d02e71056b342ebe88
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-28 16:25:51 +0530

    Time: 148 ms (66.08%), Space: 51.5 MB (54.23%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.js
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.js
@@ -2,8 +2,8 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-const findDisappearedNumbers = (nums) => {
-    const ans = Array(nums.length).fill().map((_, i) => i + 1);
+const findDisappearedNumbers = nums => {
+    const ans = new Array(nums.length).fill().map((_, i) => i + 1);
     
     for (let i = 0, n = nums.length; i < n; i++)
         ans[nums[i] - 1] = false;
