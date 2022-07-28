b7a68b7bd8f9be79edf93f1f85593d249a4806d0
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-28 17:17:10 +0530

    Time: 308 ms (12.08%), Space: 105 MB (8.69%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.js
+++ 41-first-missing-positive/41-first-missing-positive.js
@@ -6,7 +6,7 @@ var firstMissingPositive = function (nums) {
     if (!nums.includes(1))
         return 1;
     
-    nums = [...new Set(nums)].filter(n => n > 0).sort((a, b) => a - b);
+    nums = Array.from(new Set(nums)).filter(n => n > 0).sort((a, b) => a - b);
     
     const max = nums.at(-1) + 1;
     const min = nums[0];
