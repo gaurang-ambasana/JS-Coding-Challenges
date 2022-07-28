1cc1b1009819fe9ee854b1c73e5373351c34fde6
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-28 17:13:47 +0530

    Time: 278 ms (16.80%), Space: 105.5 MB (8.25%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.js
+++ 41-first-missing-positive/41-first-missing-positive.js
@@ -2,7 +2,7 @@
  * @param {number[]} nums
  * @return {number}
  */
-var firstMissingPositive = (nums) => {
+const firstMissingPositive = nums => {
     if (!nums.includes(1))
         return 1;
     
