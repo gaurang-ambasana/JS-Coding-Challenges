52565cc243e17b14f3daf32d0c9e60f1f2db418e
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-28 15:40:02 +0530

    Time: 567 ms (44.37%), Space: 106.2 MB (39.82%) - LeetHub

---
+++ 2295-replace-elements-in-an-array/2295-replace-elements-in-an-array.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number[][]} operations
+ * @return {number[]}
+ */
+var arrayChange = function(nums, operations) {
+    const map = nums.reduce((o, n, i) => (o[n] = i, o), {});
+    
+    for (const [org, num] of operations) {
+        nums[map[org]] = num;
+        map[num] = map[org];
+    }
+    
+    return nums;
+};
