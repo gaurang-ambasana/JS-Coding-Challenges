8b6c472d1b9cd87fb592f8632074c527be0cb1fe
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-28 17:39:31 +0530

    Time: 396 ms (6.94%), Space: 54.2 MB (40.67%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.js
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.js
@@ -2,4 +2,4 @@
  * @param {number[]} nums
  * @return {number}
  */
-const findDuplicate = nums => nums.sort((a, b) => a - b).find((n, i) => n - nums[i + 1] === 0);
+const findDuplicate = nums => nums.sort((a, b) => a - b).reverse().find((n, i) => n - nums[i + 1] === 0);
