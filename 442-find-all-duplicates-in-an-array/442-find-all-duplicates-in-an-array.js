ba76d34ed6d6f2dbcb590bd4ad8e5236c548b992
Gaurang Ambasana <gpambasana@gmail.com>
2022-07-28 17:29:58 +0530

    Time: 177 ms (38.35%), Space: 57.8 MB (15.50%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.js
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.js
@@ -2,6 +2,4 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-const findDuplicates = (nums) => {
-    return Object.entries(nums.reduce((c, n) => (c[n] = (c[n] ?? 0) + 1, c), {})).filter(([, f]) => f > 1).map(([n,]) => n);
-};
+var findDuplicates = (nums) => Object.entries(nums.reduce((c, n) => (c[n] = (c[n] ?? 0) + 1, c), {})).filter(([, f]) => f > 1).map(([n,]) => n);
