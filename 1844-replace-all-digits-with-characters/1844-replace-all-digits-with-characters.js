fb43c7b0175aab63bfedadc17a5e1d1204cb123d
Gaurang Ambasana <gpambasana@gmail.com>
2022-08-01 20:51:43 +0530

    Time: 82 ms (67.03%), Space: 42.3 MB (45.29%) - LeetHub

--- 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.js
+++ 1844-replace-all-digits-with-characters/1844-replace-all-digits-with-characters.js
@@ -2,4 +2,4 @@
  * @param {string} s
  * @return {string}
  */
-var replaceDigits = (s) => s.split(``).map((c, i) => isNaN(c) ? c : String.fromCharCode(s.charCodeAt(i - 1) + parseInt(c))).join(``);
+var replaceDigits = s => s.split(``).map((c, i) => isNaN(c) ? c : String.fromCharCode(s.charCodeAt(i - 1) + parseInt(c))).join(``);
