fdec9ec50cff28072ce23d93190005ca6fb30407
Gaurang Ambasana <gpambasana@gmail.com>
2022-08-01 22:39:59 +0530

    Time: 93 ms (39.56%), Space: 42.3 MB (25.47%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {character}
+ */
+var repeatedCharacter = (s) => {
+    const set = new Set();
+    
+    for (let i = 0, n = s.length; i < n; i++)
+        if (set.has(s[i]))
+            return s[i];
+        else set.add(s[i]);
+};
