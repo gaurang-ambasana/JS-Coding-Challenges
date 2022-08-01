0a4fcf3532a85facf9bc8fc5c81d2efd7e397d15
Gaurang Ambasana <gpambasana@gmail.com>
2022-08-01 18:39:34 +0530

    Time: 214 ms (21.52%), Space: 63.7 MB (5.08%) - LeetHub

--- 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.js
+++ 1684-count-the-number-of-consistent-strings/1684-count-the-number-of-consistent-strings.js
@@ -3,4 +3,4 @@
  * @param {string[]} words
  * @return {number}
  */
-var countConsistentStrings = (allowed, words) => words.map(w => Array.from(new Set(w)).filter(c => !allowed.includes(c)).join(``)).filter(w => !w).length;
+var countConsistentStrings = (allowed, words) => words.map(w => Array.from(new Set(w)).filter(c => !allowed.includes(c))).filter(({ length }) => !length).length;
