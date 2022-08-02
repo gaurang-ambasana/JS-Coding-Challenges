71588997ec11a0a83c06248269f57133913a2c24
Gaurang Ambasana <gpambasana@gmail.com>
2022-08-02 23:57:07 +0530

    Time: 2197 ms (5.33%), Space: 48.7 MB (15.43%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    if (nums.slice(1).reduce((s, e) => s + e, 0) === 0)
+        return 0;
+        
+    for (let i = 1, n = nums.length; i < n; i++)
+        if (nums.slice(0, i).reduce((s, e) => s + e, 0) === nums.slice(i + 1).reduce((s, e) => s + e, 0))
+            return i;
+    
+    return -1;
+};
