2ae9445063e65a10024b5ce881d27c9233c34807
Gaurang Ambasana <gpambasana@gmail.com>
2022-08-02 23:14:06 +0530

    Time: 57 ms (96.86%), Space: 42.3 MB (25.09%) - LeetHub

--- 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.js
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.js
@@ -3,7 +3,7 @@
  * @param {character} ch
  * @return {string}
  */
-var reversePrefix = (word, ch) => {
+const reversePrefix = (word, ch) => {
     const idx = word.indexOf(ch) + 1;
-    return word.slice(0, idx).split(``).reverse().join(``).concat(word.slice(idx));
+    return word.slice(0, idx).split(``).reverse().join(``) + word.slice(idx);
 }
