179818c035d6effda610475a231f566d526afd08
Gaurang Ambasana <gpambasana@gmail.com>
2022-08-03 11:51:04 +0530

    Time: 101 ms (67.40%), Space: 44.6 MB (30.78%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.js
+++ 205-isomorphic-strings/205-isomorphic-strings.js
@@ -7,7 +7,7 @@ var isIsomorphic = (s, t) => {
     const hash1 = {}, hash2 = {};
     
     for (let i = 0, n = s.length; i < n; i++) {
-        const c1 = s[i], c2 = t[i];
+        const [c1, c2] = [s[i], t[i]];
         
         if (!hash2[c2] && !hash1[c1])
             [hash1[c1], hash2[c2]] = [c2, c1];
