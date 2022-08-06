29bae4adeec5b90388742d4e3f9429ed5e75fdfc
Gaurang Ambasana <gpambasana@gmail.com>
2022-08-06 22:49:37 +0530

    Time: 116 ms (81.08%), Space: 46.6 MB (62.16%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.js
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.js
@@ -2,11 +2,8 @@
  * @param {string} directions
  * @return {number}
  */
-var countCollisions = (directions) => {
-  let start,
-    end,
-    counter = 0,
-    n = directions.length;
+const countCollisions = directions => {
+  let start, end, counter = 0, n = directions.length;
 
   for (let i = 0; i < n; i++)
     if (directions[i] !== `L`) {
@@ -21,8 +18,8 @@ var countCollisions = (directions) => {
     }
 
   for (let i = start; i < end + 1; i++) 
-      if (directions[i] !== `S`)
-          counter++;
+    if (directions[i] !== `S`)
+      counter++;
 
   return counter;
-};
+}
