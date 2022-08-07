a1f62d4169db79149ebcd5972b1cf02cc37bf5ef
Gaurang Ambasana <gpambasana@gmail.com>
2022-08-08 00:54:55 +0530

    Time: 85 ms (61.64%), Space: 42.4 MB (18.53%) - LeetHub

--- 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.js
+++ 1812-determine-color-of-a-chessboard-square/1812-determine-color-of-a-chessboard-square.js
@@ -2,4 +2,4 @@
  * @param {string} coordinates
  * @return {boolean}
  */
-var squareIsWhite = coordinates => coordinates.charCodeAt(0) % 2 !== coordinates[1] % 2;
+const squareIsWhite = coordinates => coordinates.charCodeAt(0) % 2 !== coordinates[1] % 2;
