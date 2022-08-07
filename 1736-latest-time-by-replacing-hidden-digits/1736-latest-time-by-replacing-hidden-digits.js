ee1b73cc60456c34fe0e815cd265832ab8c03dc1
Gaurang Ambasana <gpambasana@gmail.com>
2022-08-08 00:26:01 +0530

    Time: 137 ms (8.93%), Space: 42.3 MB (25.00%) - LeetHub

--- 1736-latest-time-by-replacing-hidden-digits/1736-latest-time-by-replacing-hidden-digits.js
+++ 1736-latest-time-by-replacing-hidden-digits/1736-latest-time-by-replacing-hidden-digits.js
@@ -2,39 +2,13 @@
  * @param {string} time
  * @return {string}
  */
-var maximumTime = time => {
+const maximumTime = time => {
     time = time.split(``);
     
-    while (time.includes(`?`)) {
-        const idx = time.indexOf(`?`);
-        
-        switch (idx) {
-            case 0: {
-                if (parseInt(time[1]) > 3)
-                    time[0] = 1;
-                else time[0] = 2;
-                break;
-            }
-                
-            case 1: {
-                if (parseInt(time[0]) === 0 || parseInt(time[0]) === 1)
-                    time[1] = 9;
-                else if (parseInt(time[0]) === 2)
-                    time[1] = 3;
-                break;
-            }
-                
-            case 3: {
-                time[3] = 5;
-                break;
-            }
-            
-            case 4: {
-                time[4] = 9;
-                break;
-            }
-        }
-    }
+    if (time[0] === `?`) time[0] = time[1] > 3 ? 1 : 2;
+    if (time[1] === `?`) time[1] = time[0] > 1 ? 3 : 9;
+    if (time[3] === `?`) time[3] = 5;
+    if (time[4] === `?`) time[4] = 9;
     
     return time.join(``);
 };
