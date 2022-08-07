b5212b991ee7082f3f465aa38af8eaad571ed778
Gaurang Ambasana <gpambasana@gmail.com>
2022-08-08 00:39:59 +0530

    Time: 85 ms (80.62%), Space: 44 MB (68.51%) - LeetHub

--- 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.js
+++ 1941-check-if-all-characters-have-equal-number-of-occurrences/1941-check-if-all-characters-have-equal-number-of-occurrences.js
@@ -2,4 +2,4 @@
  * @param {string} s
  * @return {boolean}
  */
-var areOccurrencesEqual = s => new Set(Object.values(s.split(``).reduce((a, c) => (a[c] = (a[c] ?? 0) + 1, a), {}))).size === 1;
+const areOccurrencesEqual = s => new Set(Object.values(s.split(``).reduce((a, c) => (a[c] = (a[c] ?? 0) + 1, a), {}))).size === 1;
