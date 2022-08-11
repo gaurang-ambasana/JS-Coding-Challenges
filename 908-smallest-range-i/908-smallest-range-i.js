681dca87d7b5f100f7222ee967b2dad6913e8278
Gaurang Ambasana <gpambasana@gmail.com>
2022-08-11 12:22:43 +0530

    Time: 91 ms (60.46%), Space: 43.9 MB (80.23%) - LeetHub

--- 908-smallest-range-i/908-smallest-range-i.js
+++ 908-smallest-range-i/908-smallest-range-i.js
@@ -3,4 +3,4 @@
  * @param {number} k
  * @return {number}
  */
-var smallestRangeI = (nums, k) => Math.max(0, Math.max.apply(null, nums) - Math.min.apply(null, nums) - 2 * k);
+const smallestRangeI = (nums, k) => Math.max(0, Math.max.apply(null, nums) - Math.min.apply(null, nums) - 2 * k);
