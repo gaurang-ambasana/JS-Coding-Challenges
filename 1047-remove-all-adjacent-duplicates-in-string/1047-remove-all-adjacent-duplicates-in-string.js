bb4966684704b73aa46f17b67cf34f08b00a24f1
Gaurang Ambasana <gpambasana@gmail.com>
2022-08-11 14:45:55 +0530

    Time: 1012 ms (5.08%), Space: 77.5 MB (5.08%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.js
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.js
@@ -2,4 +2,4 @@
  * @param {string} s
  * @return {string}
  */
-var removeDuplicates = (s) => s.split(``).slice(1).reduce((str, c) => str.at(-1) === c ? str.slice(0, -1) : (str += c), s[0]);
+var removeDuplicates = (s) => s.split(``).slice(1).reduce((str, c) => str.at(-1) === c ? str.slice(0, -1) : str.concat(c), s[0]);
