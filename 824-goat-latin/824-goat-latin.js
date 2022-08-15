f51ffc0090694a6a64f689ae545177bc93bbaf8c
Gaurang Ambasana <gpambasana@gmail.com>
2022-08-15 15:16:01 +0530

    Time: 93 ms (47.62%), Space: 41.7 MB (99.32%) - LeetHub

---
+++ 824-goat-latin/824-goat-latin.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} sentence
+ * @return {string}
+ */
+var toGoatLatin = sentence => {
+    const words = sentence.split(` `);
+    
+    for (let i = 0, n = words.length; i < n; i++) {
+        if ([`a`, `e`, `i`, `o`, `u`].includes(words[i][0].toLowerCase()))
+            words[i] += 'ma';
+        else words[i] = words[i].slice(1) + words[i].slice(0, 1) + 'ma';
+        
+        words[i] += `a`.repeat(i + 1);
+    }
+    
+    return words.join(` `);
+};
