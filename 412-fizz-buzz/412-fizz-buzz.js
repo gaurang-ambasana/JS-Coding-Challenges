fd49f5b26d5284ca27d6f8e06bfe1c7194dcc953
Gaurang Ambasana <gpambasana@gmail.com>
2022-08-15 14:43:16 +0530

    Time: 105 ms (44.16%), Space: 44.8 MB (14.12%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.js
+++ 412-fizz-buzz/412-fizz-buzz.js
@@ -2,6 +2,6 @@
  * @param {number} n
  * @return {string[]}
  */
-var fizzBuzz = function(n) {
+const fizzBuzz = function(n) {
     return [...Array(n + 1).keys()].slice(1).map(x => x % 3 === 0 && x % 5 === 0 ? 'FizzBuzz' : x % 3 === 0 ? 'Fizz' : x % 5 ? x.toString() : 'Buzz');
 }
