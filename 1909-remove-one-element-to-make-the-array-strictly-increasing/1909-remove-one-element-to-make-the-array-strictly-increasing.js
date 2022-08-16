bba006f9b7628288b4cb624fd3c306716da53621
Gaurang Ambasana <gpambasana@gmail.com>
2022-08-16 23:50:54 +0530

    Time: 801 ms (7.59%), Space: 48.8 MB (15.51%) - LeetHub

--- 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.js
+++ 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.js
@@ -2,7 +2,7 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-const canBeIncreasing = nums => {
+const canBeIncreasing = (nums) => {
     for (let i = 0, n = nums.length; i < n; i++) {
         const arr = [...nums];
         arr.splice(i, 1);
