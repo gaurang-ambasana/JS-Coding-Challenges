d33c8630a9b3253e75bfbf910a2faca97a2b86b2
Gaurang Ambasana <gpambasana@gmail.com>
2022-08-18 18:35:46 +0530

    Time: 1297 ms (31.71%), Space: 112.8 MB (29.27%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.js
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.js
@@ -5,10 +5,10 @@
  */
 var findKthBit = function(n, k) {
     const arr = {
-        0: "0",
+        1: "0",
     };
     
-    for (let i = 1, z = n + 1; i < z; i++) {
+    for (let i = 2, z = n + 1; i < z; i++) {
         const s = arr[i - 1].toString();
         arr[i] = s + "1" + s.split(``).map(x => 1 - x).reverse().join(``);
     }
