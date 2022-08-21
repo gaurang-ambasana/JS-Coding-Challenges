aac06c1c5bbc33245cc57ee6ecb67c05ea33ef59
Gaurang Ambasana <gpambasana@gmail.com>
2022-08-21 23:45:39 +0530

    Time: 147 ms (26.32%), Space: 48.9 MB (28.95%) - LeetHub

--- 748-shortest-completing-word/748-shortest-completing-word.js
+++ 748-shortest-completing-word/748-shortest-completing-word.js
@@ -6,7 +6,7 @@
 var shortestCompletingWord = (licensePlate, words) => {
     const chars = licensePlate.replaceAll(/[0-9]/g, '').replaceAll(/\s/g, '').toLowerCase().split(``);
     const freq = chars.reduce((c, w) => (c[w] = (c[w] ?? 0) + 1, c), {});
-                                                                                   const freqOfWords = words.map(x => x.split(``).reduce((c, w) => (c[w] = (c[w] ?? 0) + 1, c), {}));
+    const freqOfWords = words.map(x => x.split(``).reduce((c, w) => (c[w] = (c[w] ?? 0) + 1, c), {}));
     
     return words.reduce((ans, w, i) => {
         const counter = freqOfWords[i];
