241357beb4e0ce6cf4e0dfe4d0737840108191f1
Gaurang Ambasana <gpambasana@gmail.com>
2022-08-22 12:30:08 +0530

    Time: 110 ms (46.21%), Space: 43.6 MB (50.27%) - LeetHub

--- 342-power-of-four/342-power-of-four.js
+++ 342-power-of-four/342-power-of-four.js
@@ -2,6 +2,6 @@
  * @param {number} n
  * @return {boolean}
  */
-var isPowerOfFour = function(n) {
+var isPowerOfFour = (n) => {
     return n === 1 ? true : n % 4 === 0 && n !== 0 ? isPowerOfFour(n/4) : false;
 };
