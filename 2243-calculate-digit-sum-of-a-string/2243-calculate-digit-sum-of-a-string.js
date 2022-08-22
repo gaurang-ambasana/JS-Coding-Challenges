799650bdade10bb468bc6dd8df0f7655ab947f0d
Gaurang Ambasana <gpambasana@gmail.com>
2022-08-22 12:29:10 +0530

    Time: 123 ms (7.83%), Space: 42.7 MB (29.57%) - LeetHub

--- 2243-calculate-digit-sum-of-a-string/2243-calculate-digit-sum-of-a-string.js
+++ 2243-calculate-digit-sum-of-a-string/2243-calculate-digit-sum-of-a-string.js
@@ -4,9 +4,10 @@
  * @return {string}
  */
 const digitSum = (s, k) => {
+    const getSum = arr => arr.reduce((a, n) => parseInt(a) + parseInt(n), 0).toString();
+    const regex = new RegExp(`.{1,${k}}`, `g`);
     while (s.length > k) {
-        const getSum = arr => arr.reduce((a, n) => parseInt(a) + parseInt(n), 0).toString();
-        const parts = s.match(new RegExp(`.{1,${k}}`, `g`));
+        const parts = s.match(regex);
         s = parts.map(p => getSum(p.split(``))).join(``);
     }
     
