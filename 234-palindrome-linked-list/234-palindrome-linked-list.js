b1ff36c5921900460fc808f0dc2029ea2cb8d836
Gaurang Ambasana <gpambasana@gmail.com>
2022-08-23 11:41:50 +0530

    Time: 298 ms (15.85%), Space: 88.4 MB (13.41%) - LeetHub

---
+++ 234-palindrome-linked-list/234-palindrome-linked-list.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var isPalindrome = function(head) {
+    const arr = [head.val];
+    
+    let node = head.next;
+    
+    while (node) {
+        arr.push(node.val);
+        node = node.next;
+    }
+    
+    return arr.join() === arr.reverse().join();
+};
