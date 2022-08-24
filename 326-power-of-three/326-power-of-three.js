e821b2d441f0189831cae7186d414f394841ead0
Gaurang Ambasana <gpambasana@gmail.com>
2022-08-24 11:58:01 +0530

    Time: 361 ms (37.71%), Space: 50.8 MB (82.90%) - LeetHub

--- 326-power-of-three/326-power-of-three.js
+++ 326-power-of-three/326-power-of-three.js
@@ -2,4 +2,4 @@
  * @param {number} n
  * @return {boolean}
  */
-var isPowerOfThree = (n) => n === 1 ? true : n % 3 === 0 && n !== 0 ? isPowerOfThree(n/3) : false;
+var isPowerOfThree = n => n === 1 ? true : n % 3 === 0 && n !== 0 ? isPowerOfThree(n/3) : false;
