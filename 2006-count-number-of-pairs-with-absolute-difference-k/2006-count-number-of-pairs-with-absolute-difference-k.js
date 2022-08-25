f6d67908922ea0c74697605d471ddad2beb05784
Gaurang Ambasana <gpambasana@gmail.com>
2022-08-25 21:56:57 +0530

    Time: 106 ms (45.44%), Space: 42.3 MB (84.97%) - LeetHub

--- 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.js
+++ 2006-count-number-of-pairs-with-absolute-difference-k/2006-count-number-of-pairs-with-absolute-difference-k.js
@@ -3,9 +3,7 @@
  * @param {number} k
  * @return {number}
  */
-const countKDifference = (nums, k) => {
-    let count = 0;
-    
+const countKDifference = (nums, k, count = 0) => {
     for (let i = 0, n = nums.length; i < n; i++)
         for (let j = i + 1; j < n; j++)
             if (Math.abs(nums[i] - nums[j]) === k)
