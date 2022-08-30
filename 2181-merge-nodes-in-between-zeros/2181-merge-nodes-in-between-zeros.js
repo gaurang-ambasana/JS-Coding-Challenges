8c7dfe98554a54b8d5ce2a643f9f51462b9ed1d9
Gaurang Ambasana <gpambasana@gmail.com>
2022-08-30 23:17:26 +0530

    Time: 6023 ms (5.20%), Space: 157.2 MB (5.74%) - LeetHub

---
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.js
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var mergeNodes = function(head) {
+    let count = 0, node = head.next;
+    
+    const arr = [];
+    
+    while (node) {
+        const { val, next } = node;
+        
+        if (val) count += val;
+        else {
+            arr.unshift(count);
+            count = 0;
+        }
+        
+        node = next;
+    }
+
+    return arr.reduce((head, val) => new ListNode(val, head), null);
+}
