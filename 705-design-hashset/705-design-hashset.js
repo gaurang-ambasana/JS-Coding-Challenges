e50980f9026938bad56c9c7683be0eef6bd45c37
Gaurang Ambasana <gpambasana@gmail.com>
2022-08-30 22:12:24 +0530

    Time: 259 ms (52.83%), Space: 52.1 MB (69.33%) - LeetHub

--- 705-design-hashset/705-design-hashset.js
+++ 705-design-hashset/705-design-hashset.js
@@ -1,36 +1,18 @@
 
-var MyHashSet = function() {
-    this.set = new Set();
-};
-
-/** 
- * @param {number} key
- * @return {void}
- */
-MyHashSet.prototype.add = function(key) {
-    this.set.add(key);
-};
-
-/** 
- * @param {number} key
- * @return {void}
- */
-MyHashSet.prototype.remove = function(key) {
-    this.set.delete(key);
-};
-
-/** 
- * @param {number} key
- * @return {boolean}
- */
-MyHashSet.prototype.contains = function(key) {
-    return this.set.has(key);
-};
-
-/** 
- * Your MyHashSet object will be instantiated and called as such:
- * var obj = new MyHashSet()
- * obj.add(key)
- * obj.remove(key)
- * var param_3 = obj.contains(key)
- */
+class MyHashSet {
+    constructor () {
+        this.set = new Set();
+    }
+    
+    add (key) {
+        this.set.add(key);
+    }
+    
+    remove (key) {
+        this.set.delete(key);
+    }
+    
+    contains(key) {
+        return this.set.has(key);
+    }
+}
