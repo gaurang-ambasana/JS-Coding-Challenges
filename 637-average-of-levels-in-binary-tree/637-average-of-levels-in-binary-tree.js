a545f12ef176dbb0030b15c671f5b3d121f30701
Gaurang Ambasana <gpambasana@gmail.com>
2022-09-02 17:52:04 +0530

    Time: 115 ms (60.03%), Space: 47 MB (68.74%) - LeetHub

---
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.js
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var averageOfLevels = function(root) {
+    const ans = [];
+    const queue = [root];
+    
+    let sum = 0, count = 0, tmp = [];
+    
+    while (queue.length) {
+        const { val, left, right } = queue.shift();
+        
+        sum += val;
+        count += 1;
+        
+        left && tmp.push(left);
+        right && tmp.push(right);
+        
+        if (queue.length === 0) {
+            ans.push(sum / count);
+            queue.push(...tmp);
+            
+            count = 0;
+            sum = 0;
+            tmp.length = 0;
+        }
+    }
+    
+    return ans;
+};
