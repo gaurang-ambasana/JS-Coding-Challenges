74f3c15a2f230faf7029f9e7d6bc89ae181117a0
Gaurang Ambasana <gpambasana@gmail.com>
2022-09-06 19:04:34 +0530

    Time: 96 ms (33.09%), Space: 42.9 MB (12.50%) - LeetHub

---
+++ 814-binary-tree-pruning/814-binary-tree-pruning.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+function pruneTree(root) {
+    if (root === null) return null;
+
+    if (!pruneTree(root.left))
+        root.left = null;
+    
+    if (!pruneTree(root.right))
+        root.right = null;
+    
+    if (root.val || root.left || root.right)
+        return root;
+    
+    return null;
+};
