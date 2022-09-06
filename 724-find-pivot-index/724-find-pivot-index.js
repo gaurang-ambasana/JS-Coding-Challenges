cc3a32066d18f131dde7098deb31800b95c9fb7a
Gaurang Ambasana <gpambasana@gmail.com>
2022-09-06 19:53:52 +0530

    Time: 82 ms (90.94%), Space: 44.1 MB (92.32%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.js
+++ 724-find-pivot-index/724-find-pivot-index.js
@@ -5,7 +5,7 @@
 function pivotIndex(nums) {
     const total = nums.reduce((s, n) => s + n, 0);
     
-    let runningSum = 0
+    let runningSum = 0;
     
     return nums.findIndex(n => {
         if (runningSum === total - n - runningSum)
