53f2c0ea8ecbb9ed3ab77a1f4fe36594eab8d5ce
Gaurang Ambasana <gpambasana@gmail.com>
2022-09-07 16:30:32 +0530

    Time: 92 ms (48.53%), Space: 41.8 MB (84.93%) - LeetHub

--- 392-is-subsequence/392-is-subsequence.js
+++ 392-is-subsequence/392-is-subsequence.js
@@ -3,7 +3,7 @@
  * @param {string} t
  * @return {boolean}
  */
-const isSubsequence = (s, t) => {
+var isSubsequence = (s, t) => {
     for (let i = 0, n = s.length; i < n; i++) {
         const idx = t.indexOf(s[i]) + 1;      
         if (idx) t = t.slice(idx);
