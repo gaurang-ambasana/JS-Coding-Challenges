7f6ad803d7fb4859705b8d9ed40eab7a06255b14
Gaurang Ambasana <gpambasana@gmail.com>
2022-09-07 16:21:35 +0530

    Time: 111 ms (54.45%), Space: 46.1 MB (23.01%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.js
+++ 205-isomorphic-strings/205-isomorphic-strings.js
@@ -5,17 +5,17 @@
  */
 function isIsomorphic(s, t) {
     function getPattern(str) {
-        const map = {};
+        const map = new Map();
         
         let id = 0, pattern = ``;
         
         for (let i = 0, n = str.length; i < n; i++) {
             const char = str[i];
             
-            if (!(char in map))
-                map[char] = ++id;
+            if (!(map.has(char)))
+                map.set(char, ++id);
             
-            pattern += map[char];
+            pattern += map.get(char);
         }
         
         return pattern;
