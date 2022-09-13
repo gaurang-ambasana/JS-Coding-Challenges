a02b6c8b178ec86e161a54109618510ec88069f4
Gaurang Ambasana <gpambasana@gmail.com>
2022-09-13 22:08:38 +0530

    Time: 108 ms (31.66%), Space: 44.9 MB (69.08%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -1,1 +1,15 @@
-const search = (nums, target) => nums.indexOf(target);
+const search = (nums, target) => {
+    let left = 0, right = nums.length - 1;
+    
+    while (left <= right) {
+        const mid = Math.floor((right + left) / 2);
+            
+        if (nums[mid] > target)
+            right = mid - 1;
+        else if (nums[mid] < target)
+            left = mid + 1;
+        else return mid;
+    }
+    
+    return -1;
+};
