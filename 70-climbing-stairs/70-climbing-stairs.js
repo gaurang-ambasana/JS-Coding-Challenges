df51c30489e0521afc2b45e5f773613a688a86e4
Gaurang Ambasana <gpambasana@gmail.com>
2022-09-15 19:30:16 +0530

    Time: 92 ms (40.26%), Space: 42.1 MB (18.56%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    const a = [1, 1], x = n + 1;
+    
+    while (a.length !== n + 1) 
+        a.push(a.at(-1) + a.at(-2));
+    
+    return a[n];
+};
