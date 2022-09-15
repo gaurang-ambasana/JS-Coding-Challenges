84ca60eb9c3dc8be2b437f14bc8a8b247f110794
Gaurang Ambasana <gpambasana@gmail.com>
2022-09-15 23:08:00 +0530

    Time: 736 ms (31.25%), Space: 87.5 MB (6.25%) - LeetHub

---
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} changed
+ * @return {number[]}
+ */
+function findOriginalArray(changed) {
+    if (changed.length % 2 !== 0) return [];
+    
+    changed.sort((a, b) => a - b);
+    
+    const map = {}, ans = [];
+    
+    for (let i = 0, n = changed.length; i < n; i++) {
+        const curr = changed[i];
+        
+        if (curr in map) {
+            const freq = map[curr];
+            
+            if (freq === 1)
+                delete map[curr];
+            else map[curr] = freq - 1;
+        } else {
+            const double = curr * 2;
+            map[double] = (map[double] ?? 0) + 1;
+            ans.push(curr);
+        }
+    }
+    
+    return Object.values(map).length > 0 ? [] : ans;
+};
