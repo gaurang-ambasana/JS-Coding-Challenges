9941902d50b5cf22de7d907f93c9ccd150fd73ff
Gaurang Ambasana <gpambasana@gmail.com>
2022-09-17 23:13:55 +0530

    Time: 149 ms (5.21%), Space: 42.6 MB (77.57%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
@@ -2,9 +2,9 @@
  * @param {number[]} cost
  * @return {number}
  */
-var minCostClimbingStairs = function(cost) {
+function minCostClimbingStairs(cost) {
     for (let i = cost.length - 3; i > -1; i--)
         cost[i] += Math.min(cost[i + 1], cost[i + 2]);
     
     return Math.min.apply(null, cost.slice(0, 2));
-};
+}
