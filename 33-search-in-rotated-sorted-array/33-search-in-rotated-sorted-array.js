0cd045072496ea4da5463104c46dad53bfc32df4
Gaurang Ambasana <gpambasana@gmail.com>
2022-09-21 14:28:46 +0530

    Time: 79 ms (72.09%), Space: 46.5 MB (5.15%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -7,15 +7,10 @@ var search = function(nums, target) {
     if (nums.includes(target)) { 
         let n = nums.length;
         const initialIdx = nums.indexOf(target);
-    
-        if (JSON.stringify(nums) === JSON.stringify([...nums].sort((a, b) => a - b)))
-            return initialIdx;
         
-        while (n--) {
-            nums.push(nums.shift());
-            if (JSON.stringify(nums) === JSON.stringify([...nums].sort((a, b) => a - b)))
-                return initialIdx;
-        }
+        while (n--)
+            if (JSON.stringify(nums) === JSON.stringify([...nums].sort((a, b) => a - b))) return initialIdx;
+            else nums.push(nums.shift());
     
         return -1;
     } else return -1;
