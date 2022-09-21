4f19089ef37151e7c7400747e13de771cfb45984
Gaurang Ambasana <gpambasana@gmail.com>
2022-09-21 11:33:40 +0530

    Time: 8333 ms (6.06%), Space: 82.7 MB (6.06%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.js
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.js
@@ -3,9 +3,7 @@
  * @param {number[][]} queries
  * @return {number[]}
  */
-var sumEvenAfterQueries = function(nums, queries) {
-    return queries.map(([n, i]) => {
-        nums[i] += n;
-        return nums.filter(x => x % 2 === 0).reduce((sum, x) => sum + x, 0);
-    });
-};
+var sumEvenAfterQueries = (nums, queries) => queries.map(([n, i]) => {
+    nums[i] += n;
+    return nums.filter(x => x % 2 === 0).reduce((sum, x) => sum + x, 0);
+});
