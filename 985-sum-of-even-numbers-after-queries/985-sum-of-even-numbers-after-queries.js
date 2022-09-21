2d6597808a08cf7af70d2579c8a1fd8ea69bcf07
Gaurang Ambasana <gpambasana@gmail.com>
2022-09-21 11:48:49 +0530

    Time: 184 ms (65.15%), Space: 50.7 MB (50.00%) - LeetHub

--- 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.js
+++ 985-sum-of-even-numbers-after-queries/985-sum-of-even-numbers-after-queries.js
@@ -3,7 +3,18 @@
  * @param {number[][]} queries
  * @return {number[]}
  */
-var sumEvenAfterQueries = (nums, queries) => queries.map(([n, i]) => {
-    nums[i] += n;
-    return nums.filter(x => x % 2 === 0).reduce((sum, x) => sum + x, 0);
-});
+var sumEvenAfterQueries = (nums, queries) => {
+    let sum = nums.filter(x => x % 2 === 0).reduce((sum, x) => sum + x, 0);
+    
+    return queries.map(([n, i]) => {
+        const wasEven = nums[i] % 2 === 0;
+        nums[i] += n;
+        const isEven = nums[i] % 2 === 0;
+        
+        if (wasEven && isEven) sum += n;
+        else if (!wasEven && isEven) sum += nums[i];
+        else if (wasEven && !isEven) sum -= nums[i] - n;
+        
+        return sum;
+    });
+};                        
