9d4901d397b276a3240d3a355cc21b259c1c9cf4
Gaurang Ambasana <gpambasana@gmail.com>
2022-09-23 17:17:24 +0530

    Time: 846 ms (33.33%), Space: 105.5 MB (11.11%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.js
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.js
@@ -3,14 +3,6 @@
  * @return {number}
  */
 const concatenatedBinary = n => [...Array(n + 1).keys()].slice(2).reduce(({ ans, len }, i) => {
-    if (i === len)
-        len *= 2;
-    
-    return { 
-        ans: (ans * len + i) % 1000000007, 
-        len, 
-    };
-}, {
-    len: 4,
-    ans: 1,
-}).ans;
+    if (i === len) len *= 2;
+    return { ans: (ans * len + i) % 1000000007, len };
+}, { len: 4, ans: 1 }).ans;
