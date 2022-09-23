0c81f7b4aed62793a63f9577ff675dd8e6c93d2e
Gaurang Ambasana <gpambasana@gmail.com>
2022-09-23 17:18:57 +0530

    Time: 215 ms (77.78%), Space: 43.7 MB (55.56%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.js
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.js
@@ -3,10 +3,12 @@
  * @return {number}
  */
 var concatenatedBinary = function(n) {
-    let ans = 1, len = 4
-    for (let i = 2; i <= n; i++) {
-        if (i === len) len *= 2
+    let ans = 1, len = 4;
+    
+    for (let i = 2, x = n + 1; i < x; i++) {
+        if (i === len) len *= 2;
         ans = (ans * len + i) % 1000000007;
     }
-    return ans
+    
+    return ans;
 };
