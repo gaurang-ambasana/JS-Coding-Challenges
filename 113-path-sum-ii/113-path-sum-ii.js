f775e2b290ad91b4e25f49177ed25bb61bce472c
Gaurang Ambasana <gpambasana@gmail.com>
2022-09-25 03:08:45 +0530

    Time: 148 ms (16.07%), Space: 53.3 MB (23.80%) - LeetHub

---
+++ 113-path-sum-ii/113-path-sum-ii.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {number[][]}
+ */
+var pathSum = function(root, targetSum) {
+    const ans = [];
+    
+    function traverse(node, path, diff) {
+        const { val, left, right } = node;
+        
+        if (diff === val && !left && !right) {
+            ans.push([...path, val]);
+            return;
+        }
+        
+        left && traverse(left, [...path, val], diff - val);
+        right && traverse(right, [...path, val], diff - val);
+    }
+    
+    root && traverse(root, [], targetSum);
+    
+    return ans;
+};
