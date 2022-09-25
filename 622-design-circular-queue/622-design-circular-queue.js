07241d1c13eb997244c858ae805e0860e3b02eb7
Gaurang Ambasana <gpambasana@gmail.com>
2022-09-25 19:03:12 +0530

    Time: 110 ms (95.64%), Space: 50.2 MB (31.03%) - LeetHub

--- 622-design-circular-queue/622-design-circular-queue.js
+++ 622-design-circular-queue/622-design-circular-queue.js
@@ -1,62 +1,60 @@
 class MyCircularQueue {
-    /**
-     * @param {number} k
-     */
-    constructor(k) {
-        this.pointer = k;
-        this.max = k;
-        this.q = [];
-    }
-    
-    /** 
-     * @param {number} value
-     * @return {boolean}
-     */
-    enQueue(value) {
-        if (this.pointer > 0) {
-            this.pointer--;
-            this.q.push(value);
-            return true;
-        } else return false;
-    }
-    
-    
-    /**
-     * @return {boolean}
-     */
-    deQueue() {
-        if (this.q.length === 0)
-            return false;
-        this.pointer++;
-        this.q.shift();
-        return true;
-    }
-    
-    /**
-     * @return {number}
-     */
-    Front() {
-        return this.q.length ? this.q[0] : -1;
-    }
-    
-    /**
-     * @return {number}
-     */
-    Rear() {
-        return this.q.length ? this.q.at(-1) : -1;
-    }
+  /**
+   * @param {number} k
+   */
+  constructor(k) {
+    this.pointer = k;
+    this.max = k;
+    this.q = [];
+  }
 
-    /**
-     * @return {boolean}
-     */
-    isEmpty() {
-        return this.q.length === 0;
-    }
+  /**
+   * @param {number} value
+   * @return {boolean}
+   */
+  enQueue(value) {
+    if (this.pointer > 0) {
+      this.pointer--;
+      this.q.push(value);
+      return true;
+    } else return false;
+  }
 
-    /**
-     * @return {boolean}
-     */
-    isFull() {
-        return this.q.length === this.max;
-    }
+  /**
+   * @return {boolean}
+   */
+  deQueue() {
+    if (this.q.length === 0) return false;
+    this.pointer++;
+    this.q.shift();
+    return true;
+  }
+
+  /**
+   * @return {number}
+   */
+  Front() {
+    return this.q.length ? this.q[0] : -1;
+  }
+
+  /**
+   * @return {number}
+   */
+  Rear() {
+    return this.q.length ? this.q.at(-1) : -1;
+  }
+
+  /**
+   * @return {boolean}
+   */
+  isEmpty() {
+    return this.q.length === 0;
+  }
+
+  /**
+   * @return {boolean}
+   */
+  isFull() {
+    return this.q.length === this.max;
+  }
 }
