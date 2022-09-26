622a30b32badf196526614515a97803dc410cd2f
Gaurang Ambasana <gpambasana@gmail.com>
2022-09-26 17:42:55 +0530

    Time: 193 ms (56.52%), Space: 50.2 MB (100.00%) - LeetHub

--- 1670-design-front-middle-back-queue/1670-design-front-middle-back-queue.js
+++ 1670-design-front-middle-back-queue/1670-design-front-middle-back-queue.js
@@ -1,62 +1,32 @@
 
-var FrontMiddleBackQueue = function() {
-    this.q = [];
-};
+class FrontMiddleBackQueue {
+    constructor() {
+        this.q = [];
+    }
+    
+    pushFront(val) {
+        this.q.unshift(val);
+    }
 
-/** 
- * @param {number} val
- * @return {void}
- */
-FrontMiddleBackQueue.prototype.pushFront = function(val) {
-    this.q.unshift(val);
-};
-
-/** 
- * @param {number} val
- * @return {void}
- */
-FrontMiddleBackQueue.prototype.pushMiddle = function(val) {
-    this.q.splice(Math.floor(this.q.length / 2), 0, val);
-};
-
-/** 
- * @param {number} val
- * @return {void}
- */
-FrontMiddleBackQueue.prototype.pushBack = function(val) {
-    this.q.push(val);
-};
-
-/**
- * @return {number}
- */
-FrontMiddleBackQueue.prototype.popFront = function() {
-    return this.q.shift() ?? -1;
-};
-
-/**
- * @return {number}
- */
-FrontMiddleBackQueue.prototype.popMiddle = function() {
-    if (this.q.length % 2)
-        return this.q.splice(Math.floor(this.q.length / 2), 1)[0];
-    else return this.q.splice(Math.floor((this.q.length - 1) / 2), 1)[0] ?? -1;
-};
-
-/**
- * @return {number}
- */
-FrontMiddleBackQueue.prototype.popBack = function() {
-    return this.q.pop() ?? -1;
-};
-
-/** 
- * Your FrontMiddleBackQueue object will be instantiated and called as such:
- * var obj = new FrontMiddleBackQueue()
- * obj.pushFront(val)
- * obj.pushMiddle(val)
- * obj.pushBack(val)
- * var param_4 = obj.popFront()
- * var param_5 = obj.popMiddle()
- * var param_6 = obj.popBack()
- */
+    pushMiddle(val) {
+        this.q.splice(Math.floor(this.q.length / 2), 0, val);
+    }
+    
+    pushBack(val) {
+        this.q.push(val);
+    }
+    
+    popFront() {
+        return this.q.shift() ?? -1;
+    }
+        
+    popMiddle() {
+        if (this.q.length % 2)
+            return this.q.splice(Math.floor(this.q.length / 2), 1)[0];
+        else return this.q.splice(Math.floor((this.q.length - 1) / 2), 1)[0] ?? -1;
+    }
+        
+    popBack() {
+        return this.q.pop() ?? -1;
+    }
+}
