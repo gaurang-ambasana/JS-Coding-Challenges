83f42601b7763eb7e8bcd3e49e6f1e833d9bba54
Gaurang Ambasana <gpambasana@gmail.com>
2022-09-28 00:03:24 +0530

    Time: 506 ms (38.81%), Space: 66.1 MB (37.31%) - LeetHub

--- 838-push-dominoes/838-push-dominoes.js
+++ 838-push-dominoes/838-push-dominoes.js
@@ -2,7 +2,7 @@
  * @param {string} dominoes
  * @return {string}
  */
-const pushDominoes = dominoes => {
+function pushDominoes(dominoes) {
     while (true) {
         const newDominoes = dominoes.replaceAll(`R.L`, `_`).replaceAll(`.L`, `LL`).replaceAll(`R.`, `RR`).replaceAll(`_`, `R.L`);
         if (newDominoes === dominoes)
