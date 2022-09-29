e6596fef6834f7d37364132494ace23f1e7f0d7e
Gaurang Ambasana <gpambasana@gmail.com>
2022-09-29 13:25:58 +0530

    Time: 126 ms (72.64%), Space: 49.1 MB (49.09%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.js
+++ 658-find-k-closest-elements/658-find-k-closest-elements.js
@@ -4,4 +4,4 @@
  * @param {number} x
  * @return {number[]}
  */
-var findClosestElements = (arr, k, x) => arr.map(n => [n, Math.abs(n - x)]).sort(([,val1],[,val2]) => val1 - val2).slice(0, k).map(([key]) => key).sort((a, b) => a - b);
+var findClosestElements = (arr, k, x) => arr.map(n => [n, Math.abs(n - x)]).sort(([,d1],[,d2]) => d1 - d2).slice(0, k).map(([n]) => n).sort((a, b) => a - b);
