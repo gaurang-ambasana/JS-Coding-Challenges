c924b1339e295b5a136729ecdeb5d6afe8f6379d
Gaurang Ambasana <gpambasana@gmail.com>
2022-10-01 17:27:08 +0530

    Time: 101 ms (56.98%), Space: 42.2 MB (82.90%) - LeetHub

--- 91-decode-ways/91-decode-ways.js
+++ 91-decode-ways/91-decode-ways.js
@@ -2,7 +2,7 @@
  * @param {string} s
  * @return {number}
  */
-var numDecodings = function(s) {
+var numDecodings = s => {
     let dp = Array(s.length + 1).fill(0);
     dp[0] = 1;
     dp[1] = s[0] === '0' ? 0 : 1;
