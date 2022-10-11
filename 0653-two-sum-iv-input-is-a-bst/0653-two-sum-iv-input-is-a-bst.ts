2c3af605b44d381d615efc4c85a6b4c466b5d097
Gaurang Ambasana <gpambasana@gmail.com>
2022-10-11 18:07:21 +0530

    Time: 168 ms (44.30%), Space: 52.5 MB (54.36%) - LeetHub

---
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.ts
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function findTarget(root: TreeNode | null, k: number): boolean {
+    const set: Set<number> = new Set();
+    
+    let flag: boolean = false;
+    
+    function traverse(node: TreeNode): void {
+        if (flag) return;
+        
+        const { left, val, right }: any = node;
+        
+        const diff: number = k - val;
+        
+        if (set.has(diff)) {
+            flag = true;
+            return;
+        } else set.add(val);
+        
+        left && traverse(left);
+        right && traverse(right);
+    }
+
+    traverse(root);
+
+    return flag;
+};
