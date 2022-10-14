ee0dc0be10ca9556a1e0396be71a9257c8bc2ddd
Gaurang Ambasana <gpambasana@gmail.com>
2022-10-14 16:40:17 +0530

    Time: 214 ms (11.73%), Space: 49.4 MB (97.77%) - LeetHub

---
+++ 0143-reorder-list/0143-reorder-list.ts
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+/**
+ Do not return anything, modify head in-place instead.
+ */
+function reorderList(head: ListNode | null): void {
+    const arr: Array<number> = [];
+
+    let node: ListNode | null = head, i: boolean = true;
+
+    while (node) {
+        const { val, next } = node;
+        arr.push(val);
+        node = next;
+    }
+
+    node = head;
+
+    while (node) {
+        const { next } : ListNode = node;
+        
+        if (i)
+            node.val = arr.shift();
+        else node.val = arr.pop();
+        
+        i = !i;
+        node = next;
+    }
+};
