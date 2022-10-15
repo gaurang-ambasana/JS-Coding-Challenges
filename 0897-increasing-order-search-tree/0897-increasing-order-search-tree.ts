fc1bc2e1d21a3d49701edca736542347e28aea57
Gaurang Ambasana <gpambasana@gmail.com>
2022-10-15 22:29:59 +0530

    Time: 120 ms (26.67%), Space: 44.9 MB (53.33%) - LeetHub

--- 0897-increasing-order-search-tree/0897-increasing-order-search-tree.ts
+++ 0897-increasing-order-search-tree/0897-increasing-order-search-tree.ts
@@ -1,22 +1,8 @@
-/**
- * Definition for a binary tree node.
- * class TreeNode {
- *     val: number
- *     left: TreeNode | null
- *     right: TreeNode | null
- *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
- *         this.val = (val===undefined ? 0 : val)
- *         this.left = (left===undefined ? null : left)
- *         this.right = (right===undefined ? null : right)
- *     }
- * }
- */
-
 function increasingBST(root: TreeNode | null): TreeNode | null {
     const arr: Array<number> = [];
     
     function traverse(node: TreeNode) {
-        const { left, right, val } : TreeNode | null = node;
+        const { left, val, right } : TreeNode | null = node;
         
         arr.push(val);
         left && traverse(left);
