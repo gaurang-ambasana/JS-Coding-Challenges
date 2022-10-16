a27ddd766afcef1980388b737706429498a3ad94
Gaurang Ambasana <gpambasana@gmail.com>
2022-10-16 23:55:37 +0530

    Time: 304 ms (58.82%), Space: 52.2 MB (88.24%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.ts
+++ 1472-design-browser-history/1472-design-browser-history.ts
@@ -24,11 +24,3 @@ class BrowserHistory {
         return this.list[this.current];
     }
 }
-
-/**
- * Your BrowserHistory object will be instantiated and called as such:
- * var obj = new BrowserHistory(homepage)
- * obj.visit(url)
- * var param_2 = obj.back(steps)
- * var param_3 = obj.forward(steps)
- */
