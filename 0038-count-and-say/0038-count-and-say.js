7b05789f547f3e0340e1b3a28bf73c608b6533d1
Gaurang Ambasana <gpambasana@gmail.com>
2022-10-18 17:05:32 +0530

    Time: 141 ms (7.11%), Space: 47.1 MB (6.52%) - LeetHub

---
+++ 0038-count-and-say/0038-count-and-say.js
@@ -0,0 +1,26 @@
+const helper = (str) => {
+    const entries = [];
+    let count = 1, prev = str[0];
+    
+    for (let i = 1, n = str.length + 1; i < n; i++) {
+        const curr = str[i];
+
+        if (prev === curr) count++;
+        else {
+            entries.push([count, prev]);
+            prev = curr;
+            count = 1;
+        }
+    }
+
+    return entries.map((e) => e.join(``)).join(``);
+};
+
+const countAndSay = (n) => {
+  let str = `1`;
+
+  for (let i = 1; i < n; i++) 
+      str = helper(str);
+
+  return str;
+}
