a51fb563d0ef3a5115dd9dd5804f7e0e58a50c5c
Gaurang Ambasana <gpambasana@gmail.com>
2022-10-19 12:25:53 +0530

    Time: 427 ms (40.55%), Space: 61.6 MB (50.69%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.ts
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.ts
@@ -1,1 +1,1 @@
-const kClosest = (points: number[][], k: number): number[][] => points.sort(([x1, y1], [x2, y2]) => Math.sqrt((x1 ** 2) + (y1 ** 2)) - Math.sqrt((x2 ** 2) + (y2 ** 2))).slice(0, k);
+const kClosest = (points: number[][], k: number): number[][] => points.sort(([x1, y1], [x2, y2]) => Math.sqrt(Math.pow(x1, 2) + Math.pow(y1, 2)) - Math.sqrt(Math.pow(x2, 2) + Math.pow(y2, 2))).slice(0, k);
