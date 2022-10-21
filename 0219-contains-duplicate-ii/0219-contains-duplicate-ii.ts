cbb5d345289c4fcf860d536366a6440541bf2890
Gaurang Ambasana <gpambasana@gmail.com>
2022-10-21 11:30:14 +0530

    Time: 279 ms (32.43%), Space: 60.3 MB (50.19%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.ts
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.ts
@@ -1,13 +1,10 @@
 function containsNearbyDuplicate(nums: number[], k: number): boolean {
     const table: { [index: number]: number } = {};
     
-    for (let i = 0, n = nums.length; i < n; i++) {
-        const j = nums[i];
-        
-        if (j in table && Math.abs(i - table[j]) <= k)
+    for (let i = 0, n = nums.length; i < n; i++)
+        if (nums[i] in table && Math.abs(i - table[nums[i]]) <= k)
             return true;
-        else table[j] = i;
-    }
+        else table[nums[i]] = i;
     
     return false;
 };
