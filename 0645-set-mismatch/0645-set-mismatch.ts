c2b29914721f7e0e6658268f23a4b0ae85169174
Gaurang Ambasana <gpambasana@gmail.com>
2022-10-25 14:57:47 +0530

    Time: 198 ms (16.22%), Space: 52.6 MB (8.11%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.ts
@@ -0,0 +1,8 @@
+function findErrorNums(nums: number[]): number[] {
+    const n: number = nums.length;
+    const duplicateNum: number = parseInt(Object.entries(nums.reduce((c, n) => (c[n] = (c[n] ?? 0) + 1, c), {})).find(([n, f]) => f === 2)[0]);
+    const sum: number = ((n * (n + 1)) / 2);
+    const missing: number = sum - <number>Array.from(new Set(nums)).reduce((sum, n) => sum + n, 0);
+    
+    return [duplicateNum, missing];
+};
