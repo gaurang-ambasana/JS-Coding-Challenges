d81f6e100bd9b428a638db387887e75ca170624d
Gaurang Ambasana <gpambasana@gmail.com>
2022-10-26 22:31:55 +0530

    Time: 94 ms (64.10%), Space: 44.3 MB (64.10%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.ts
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.ts
@@ -1,1 +1,1 @@
-var arrayStringsAreEqual = (word1: string[], word2: string[]): boolean => word1.join(``) === word2.join(``)
+var arrayStringsAreEqual = (word1: string[], word2: string[]): boolean => word1.join(``) === word2.join(``);
