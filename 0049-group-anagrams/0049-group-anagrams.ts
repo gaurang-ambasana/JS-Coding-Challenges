abf26d6a288289bab9bdf6977478ffa4ade2d677
Gaurang Ambasana <gpambasana@gmail.com>
2022-10-30 01:14:17 +0530

    Time: 170 ms (82.04%), Space: 56.4 MB (18.59%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.ts
@@ -0,0 +1,14 @@
+function groupAnagrams(strs: string[]): string[][] {
+    const map: { [index: string]: Array<string> } = {};
+                
+    for (let i = 0, n = strs.length; i < n; i++) {
+        const word = strs[i];
+        const tmp = word.split(``).sort((a, b) => a.localeCompare(b)).join(``);
+        
+        if (tmp in map)
+            map[tmp].push(word);
+        else map[tmp] = [word];
+    }
+                
+    return Object.values(map);
+}
