1940eead2dbffc61735fdee5afb152622410959d
Gaurang Ambasana <gpambasana@gmail.com>
2022-11-11 16:48:45 +0530

    Time: 191 ms (22.53%), Space: 46 MB (18.29%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.ts
@@ -1,10 +1,10 @@
 function removeDuplicates(nums: number[]): number {
-    for (let i = 1; i < nums.length; i++) {
+    for (var i = 1; i < nums.length; i++) {
         if (nums[i - 1] === nums[i]) {
             nums.splice(i, 1);
             i--;
         }
     }
 
-    return nums.length;
+    return i;
 }
