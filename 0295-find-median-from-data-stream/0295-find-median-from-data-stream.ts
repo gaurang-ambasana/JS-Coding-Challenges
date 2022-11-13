a050fdc593c44896bf6f0aa19d667f88c9c6dfab
Gaurang Ambasana <gpambasana@gmail.com>
2022-11-13 15:06:16 +0530

    Time: 2497 ms (14.28%), Space: 102.1 MB (5.95%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.ts
@@ -0,0 +1,24 @@
+class MedianFinder {
+    finder: Array<number>;
+
+    constructor() {
+        this.finder = [];
+    }
+
+    addNum(num: number): void {
+        const len = this.finder.length;
+        if (len === 0 || this.finder[len - 1] <= num)
+            this.finder.push(num);
+        else {
+            const i = this.finder.findIndex(n => n > num);
+            this.finder.splice(i, 0, num);
+        }
+    }
+
+    findMedian(): number {
+        const len = this.finder.length;
+        const mid = Math.floor(len / 2);
+        if (len % 2) return this.finder[mid];
+        else return (this.finder[mid - 1] + this.finder[mid]) / 2;
+    }
+}
