1e3803496de839d65bf8e79a8c1b91e6b02fc2e7
Gaurang Ambasana <gpambasana@gmail.com>
2022-11-25 13:52:40 +0530

    Time: 5838 ms (7.69%), Space: 48.9 MB (100.00%) - LeetHub

--- 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.ts
+++ 0907-sum-of-subarray-minimums/0907-sum-of-subarray-minimums.ts
@@ -2,10 +2,13 @@ function sumSubarrayMins(arr: number[]): number {
     let sum = 0;
 
     for (let start = 0, n = arr.length; start < n; start++) {
-        let currentMin = Number.MAX_SAFE_INTEGER;
+        let currentMin: number = arr[start];
+        sum += currentMin;
         
-        for (let i = start; i < n; i++)
-            sum += (currentMin = Math.min(arr[i], currentMin));
+        for (let i = start + 1; i < n; i++) {
+            currentMin = Math.min(arr[i], currentMin);
+            sum += currentMin;
+        }
     }
 
     return sum % 1000000007;
