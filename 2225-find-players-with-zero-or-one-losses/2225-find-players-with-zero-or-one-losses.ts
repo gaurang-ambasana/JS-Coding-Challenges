80cf1954637f161e64ad1dbf930f4d80d0639fd2
Gaurang Ambasana <gpambasana@gmail.com>
2022-11-28 16:43:59 +0530

    Time: 903 ms (14.47%), Space: 124.2 MB (17.10%) - LeetHub

---
+++ 2225-find-players-with-zero-or-one-losses/2225-find-players-with-zero-or-one-losses.ts
@@ -0,0 +1,13 @@
+function findWinners(matches: number[][]): number[][] {
+    const losers: { [index: number]: number } = {}, winners = new Set<number>([].concat(...matches));
+
+    for (let i = 0, n = matches.length; i < n; i++) {
+        const loser = matches[i][1];
+        winners.delete(loser);
+        
+        if (loser in losers) losers[loser]++;
+        else losers[loser] = 1;
+    }
+
+    return [Array.from(winners).sort((a, b) => a - b), Object.keys(losers).map(Number).filter(l => losers[l] == 1).sort((a, b) => a - b)];
+}
