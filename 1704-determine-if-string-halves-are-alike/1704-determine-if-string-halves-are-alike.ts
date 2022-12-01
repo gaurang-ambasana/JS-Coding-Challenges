12b471ac8b58767b831afaebb92524bed347f358
Gaurang Ambasana <gpambasana@gmail.com>
2022-12-01 11:57:07 +0530

    Time: 116 ms (57.89%), Space: 45 MB (42.11%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.ts
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.ts
@@ -1,3 +1,1 @@
-function halvesAreAlike(s: string): boolean {
-    return s.slice(0, s.length / 2).replace(/[^aeiou]/ig, ``).length === s.slice(s.length / 2).replace(/[^aeiou]/ig, ``).length;
-}
+const halvesAreAlike = (s: string): boolean => s.slice(0, s.length / 2).replace(/[^aeiou]/ig, ``).length === s.slice(s.length / 2).replace(/[^aeiou]/ig, ``).length;
