dbcdcd9b0b50106dd61ed1ac6a81fbddc67eb486
Gaurang Ambasana <gpambasana@gmail.com>
2022-12-03 00:11:12 +0530

    Time: 211 ms (50.00%), Space: 54 MB (50.00%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.ts
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.ts
@@ -5,9 +5,10 @@ var closeStrings = (word1: string, word2: string): boolean => {
 
     const f1 = getPattern(word1), f2 = getPattern(word2);
 
-    for (const c in f1)
+    for (const c in f1) {
         if (!(c in f2))
             return false;
+    }
 
     return Object.entries(getPattern(Object.values(f1).join(``))).sort(([, a], [, b]) => a - b).reduce((s, [c, f]) => s.concat(c.repeat(f)), ``) === Object.entries(getPattern(Object.values(f2).join(``))).sort(([, a], [, b]) => a - b).reduce((s, [c, f]) => s.concat(c.repeat(f)), ``);
 };
