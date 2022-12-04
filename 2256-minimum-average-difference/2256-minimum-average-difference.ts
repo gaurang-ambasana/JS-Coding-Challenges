3e29b87a65b51d7e938b92558fa9ae305ad1ab96
Gaurang Ambasana <gpambasana@gmail.com>
2022-12-04 17:29:14 +0530

    Time: 176 ms (37.50%), Space: 51.1 MB (100.00%) - LeetHub

--- 2256-minimum-average-difference/2256-minimum-average-difference.ts
+++ 2256-minimum-average-difference/2256-minimum-average-difference.ts
@@ -7,9 +7,9 @@ function minimumAverageDifference(nums: number[]): number {
         runningSum += nums[i];
         
         const rightSum = total - runningSum, 
-              leftAvg = Math.floor(runningSum / (i + 1)),
-              rightAvg = Math.floor(rightSum / ((i === n - 1) ? 1 : n - i - 1)),
-              diff = Math.abs(leftAvg - rightAvg);
+            leftAvg = Math.floor(runningSum / (i + 1)),
+            rightAvg = Math.floor(rightSum / ((i === n - 1) ? 1 : n - i - 1)),
+            diff = Math.abs(leftAvg - rightAvg);
 
         if (diff < minDiff) {
             minDiff = diff;
