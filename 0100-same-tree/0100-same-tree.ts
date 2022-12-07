7bc8f429a6094dd37e24be3c4a86e85708432f4f
Gaurang Ambasana <gpambasana@gmail.com>
2022-12-07 11:13:36 +0530

    Time: 107 ms (41.37%), Space: 44.9 MB (17.43%) - LeetHub

--- 0100-same-tree/0100-same-tree.ts
+++ 0100-same-tree/0100-same-tree.ts
@@ -1,11 +1,7 @@
 function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
     if (p === null && q === null) return true;
     if (p === null || q === null) return false;
+    if (p.val !== q.val) return false;
     
-    const { val: valP, left: leftP, right: rightP } = p;
-    const { val: valQ, left: leftQ, right: rightQ } = q;
-    
-    if (valP !== valQ) return false;
-    
-    return isSameTree(leftP, leftQ) && isSameTree(rightP, rightQ);
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 }
