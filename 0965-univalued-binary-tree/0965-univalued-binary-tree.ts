a98696b2233bd27f6459b994f779cf71fca73029
Gaurang Ambasana <gpambasana@gmail.com>
2022-12-07 11:28:02 +0530

    Time: 103 ms (54.05%), Space: 45.1 MB (8.11%) - LeetHub

---
+++ 0965-univalued-binary-tree/0965-univalued-binary-tree.ts
@@ -0,0 +1,18 @@
+function isUnivalTree(root: TreeNode | null): boolean {
+    const { val, left, right } = root;
+    const set = new Set<number>();
+    set.add(val);
+
+    function traverse(node: TreeNode| null): boolean {
+        if (!node) return true;
+        
+        const { val, left, right } = node;
+
+        if (!set.has(val))
+            return false;
+
+        return traverse(left) && traverse(right);
+    }
+
+    return traverse(left) && traverse(right);
+};
