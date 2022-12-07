64a61d9b5627b292be61f4d106cda776b06fab6c
Gaurang Ambasana <gpambasana@gmail.com>
2022-12-07 18:39:45 +0530

    Time: 97 ms (75.86%), Space: 46.9 MB (13.79%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.ts
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.ts
@@ -5,19 +5,16 @@ function maxAncestorDiff(root: TreeNode | null): number {
         if (!node) {
             const newDiff = max - min;
             
-            if (diff < newDiff)
-                diff = newDiff;
+            if (diff < newDiff) diff = newDiff;
             
             return;
         }
 
         const { val, left, right } = node;
 
-        if (val > max)
-            max = val;
+        if (val > max) max = val;
 
-        if (val < min)
-            min = val;
+        if (val < min) min = val;
 
         traverse(left, max, min);
         traverse(right, max, min);
