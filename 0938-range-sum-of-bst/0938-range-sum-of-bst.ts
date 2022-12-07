c94ea5846e21cc4beb616669e84e5e5f4b623f73
Gaurang Ambasana <gpambasana@gmail.com>
2022-12-07 10:33:45 +0530

    Time: 310 ms (16.85%), Space: 97.9 MB (11.24%) - LeetHub

--- 0938-range-sum-of-bst/0938-range-sum-of-bst.ts
+++ 0938-range-sum-of-bst/0938-range-sum-of-bst.ts
@@ -4,11 +4,14 @@ function rangeSumBST(root: TreeNode | null, low: number, high: number): number {
     function traverse(node: TreeNode): void {
         const { val, left, right } = node;
 
-        if (val >= low && val <= high)
+        if (val >= low && val <= high) {
             sum += val;
+            left && traverse(left);
+            right && traverse(right);
+        }
+        else if (val < low) right && traverse(right)
+        else left && traverse(left)
 
-        left && traverse(left);
-        right && traverse(right);
     }
 
     root && traverse(root);
