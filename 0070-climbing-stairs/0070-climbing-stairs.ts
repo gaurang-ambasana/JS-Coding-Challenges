2e51730d6b4843a730fb805de4ff361d3d2c1acb
Gaurang Ambasana <gpambasana@gmail.com>
2022-12-12 12:40:35 +0530

    Time: 79 ms (78.68%), Space: 43.3 MB (14.10%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.ts
+++ 0070-climbing-stairs/0070-climbing-stairs.ts
@@ -1,7 +1,7 @@
 function climbStairs(n: number): number {
     const arr = [1, 1];
 
-    for (let i = 0, x = n + 1; i < x; i++)
+    for (let i = 0; i < n + 1; i++)
         arr.push(arr.at(-1) + arr.at(-2));
 
     return arr[n];
