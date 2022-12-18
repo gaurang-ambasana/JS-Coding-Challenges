39c3d7d55d49152dea9a207712ba7c0a4ffbb53f
Gaurang Ambasana <gpambasana@gmail.com>
2022-12-18 20:06:47 +0530

    Time: 376 ms (74.30%), Space: 66.4 MB (53.63%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.ts
+++ 0739-daily-temperatures/0739-daily-temperatures.ts
@@ -2,8 +2,9 @@ function dailyTemperatures(temperatures: number[]): number[] {
     const stack = [], ans = Array(temperatures.length).fill(0);
 
     for (let i = temperatures.length - 1; i > -1; i--) {
-        while (stack.length > 0 && temperatures[stack.at(-1)] <= temperatures[i])
+        while (stack.length > 0 && temperatures[stack.at(-1)] <= temperatures[i]) {
             stack.pop();
+        }
 
         if (stack.length > 0)
             ans[i] = stack.at(-1) - i;
