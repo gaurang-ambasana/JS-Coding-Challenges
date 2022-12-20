b2496ed77848d92128559457083606ba0ce23552
Gaurang Ambasana <gpambasana@gmail.com>
2022-12-20 16:57:00 +0530

    Time: 116 ms (37.71%), Space: 44.9 MB (68.85%) - LeetHub

--- 0841-keys-and-rooms/0841-keys-and-rooms.ts
+++ 0841-keys-and-rooms/0841-keys-and-rooms.ts
@@ -2,13 +2,12 @@ function canVisitAllRooms(rooms: number[][]): boolean {
     const keys = new Set<number>(rooms[0]), visited = new Set<number>([0]);
 
     while (keys.size > 0) {
-        for (const key of keys) {
+        for (const key of keys)
             rooms[key].forEach(k => keys.add(k));
-        }
 
         for (const key of keys) {
             visited.add(key);
-            keys.delete(key)
+            keys.delete(key);
         }
     }
 
