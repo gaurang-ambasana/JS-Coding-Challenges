082541193cb4226d92e5338a8df55d43a43ab485
Gaurang Ambasana <gpambasana@gmail.com>
2022-12-21 22:33:09 +0530

    Time: 119 ms (65.21%), Space: 45.3 MB (37.69%) - LeetHub

--- 0001-two-sum/0001-two-sum.ts
+++ 0001-two-sum/0001-two-sum.ts
@@ -4,8 +4,7 @@ function twoSum(nums: number[], target: number): number[] {
     for (let i = 0, n = nums.length; i < n; i++) {
         const diff = target - nums[i];
 
-        if (map.has(diff))
-            return [map.get(diff), i];
+        if (map.has(diff)) return [map.get(diff), i];
         else map.set(nums[i], i);
     }
-};
+}
