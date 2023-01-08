5012862aa46f02612f03ba31ed7d7439e1ecb52c
Gaurang Ambasana <gpambasana@gmail.com>
2023-01-09 00:13:57 +0530

    Time: 194 ms (44.44%), Space: 59.9 MB (18.52%) - LeetHub

---
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.ts
@@ -0,0 +1,37 @@
+function maxPoints(points: number[][]): number {
+    let maxPoints = 1;
+
+    function getLineEqn(x1: number, y1: number, x2: number, y2: number): string | number {
+        const y = y2 - y1, x = x2 - x1;
+
+        if (x === 0 && y !== 0) 
+            return x2;
+
+        const prefix = y / x;
+        const c = (-x1 * prefix) + y1;
+
+        return `y=${prefix}x+${c}`;
+    }
+
+    for (let i = 1, n = points.length; i < n; i++) {
+        const lines: { [index: string]: number } = {};
+        let max = 0;
+
+        const [x1, y1] = points[i];
+
+        for (let j = 0; j < i; j++) {
+            const [x2, y2] = points[j];
+            const lineEqn = getLineEqn(x1, y1, x2, y2);
+
+            lines[lineEqn] = (lines[lineEqn] ?? 1) + 1;
+
+            if (lines[lineEqn] > max)
+                max = lines[lineEqn];
+        }
+
+        if (max > maxPoints)
+            maxPoints = max;
+    }
+
+    return maxPoints;
+}
