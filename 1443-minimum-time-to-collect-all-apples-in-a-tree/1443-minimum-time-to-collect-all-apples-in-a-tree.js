e505e9bdded2b5cd982da31bf01dff02436fc2c6
Gaurang Ambasana <gpambasana@gmail.com>
2023-01-11 23:55:34 +0530

    Time: 205 ms (70.00%), Space: 82 MB (60.00%) - LeetHub

---
+++ 1443-minimum-time-to-collect-all-apples-in-a-tree/1443-minimum-time-to-collect-all-apples-in-a-tree.js
@@ -0,0 +1,47 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {boolean[]} hasApple
+ * @return {number}
+ */
+var minTime = function(n, edges, hasApple) {
+    const map = edges.reduce((tree, [from, to]) => {
+        if (!tree.has(from))
+            tree.set(from, new Set());
+
+        if (!tree.has(to))
+            tree.set(to, new Set());
+
+        tree.get(from).add(to);
+        tree.get(to).add(from);
+
+        return tree;
+    }, new Map());
+
+    let ans = 0;
+
+    function traverse(node, parent) {
+        let flag = false;
+        const children = map.get(node);
+
+        for (const child of children) {
+            if (child !== parent) {
+                ans++;
+                
+                const tmp = traverse(child, node);
+                
+                if (tmp) ans++;
+                else ans--;
+
+                flag = tmp || flag;
+            }
+        }
+
+        if (hasApple[node]) return true;
+        else return flag;
+    }
+
+    traverse(0);
+
+    return ans;
+}
