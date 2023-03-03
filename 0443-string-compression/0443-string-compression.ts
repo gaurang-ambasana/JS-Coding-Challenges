166af974763016de9e46a63ef52e243c7a5e3617
Gaurang Ambasana <gpambasana@gmail.com>
2023-03-03 20:45:39 +0530

    Time: 78 ms (69.05%), Space: 45.2 MB (35.71%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.ts
@@ -0,0 +1,23 @@
+function compress(chars: string[]): number {
+    chars.push('_');
+    let prev = chars.shift(), count = 1;
+
+    for (let i = 1, n = chars.length + 1; i < n; i++) {
+        const curr = chars.shift();
+
+        if (prev === curr) {
+            count++;
+        } else  {
+            if (count === 1) {
+                chars.push(prev);
+            } else {
+                chars.push(prev, ...count.toString().split(``));
+                count = 1;
+            }
+
+            prev = curr;
+        }
+    }
+
+    return chars.length;
+}
