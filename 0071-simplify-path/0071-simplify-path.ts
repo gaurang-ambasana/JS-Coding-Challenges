96044cb4562f7579d04bee3ed7e0274dcad8aec6
Gaurang Ambasana <gpambasana@gmail.com>
2023-04-12 23:38:10 +0530

    Time: 62 ms (93.04%), Space: 44.7 MB (73.04%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.ts
+++ 0071-simplify-path/0071-simplify-path.ts
@@ -1,15 +1,16 @@
 function simplifyPath(path: string): string {
     const stack = [], paths = path.split(`/`);
 
-    for (const dir of paths) {
+    for (let i = 0, n = paths.length; i < n; i++) {
+        const dir = paths[i];
+        if (!dir) continue;
         if (dir === '.') continue;
         if (dir === '..') {
             stack.length > 0 && stack.pop();
             continue;
         }
-        if (dir) {
-            stack.push(dir);
-        }
+        
+        stack.push(dir);
     }
 
     return '/' + stack.join(`/`);
