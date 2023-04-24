0718753f722f8b27b44f27e39fee8daa3b7648ea
Gaurang Ambasana <gpambasana@gmail.com>
2023-04-24 16:04:57 +0530

    Time: 76 ms (9.23%), Space: 44.4 MB (20.00%) - LeetHub

---
+++ 2648-generate-fibonacci-sequence/2648-generate-fibonacci-sequence.ts
@@ -0,0 +1,13 @@
+function* fibGenerator(): Generator<number, any, number> {
+    yield 0;
+    yield 1;
+    
+    let a = 0, b = 1;
+    
+    while (true) {
+        const c = a + b;
+        [a, b] = [b, c];
+        
+        yield c;
+    }
+}
