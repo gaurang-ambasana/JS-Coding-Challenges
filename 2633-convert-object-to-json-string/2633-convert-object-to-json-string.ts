90bf1a6be01e2a7b16e75f7e1758ff1a1c17141f
Gaurang Ambasana <gpambasana@gmail.com>
2023-05-06 20:29:25 +0530

    Time: 109 ms (26.45%), Space: 54 MB (67.77%) - LeetHub

---
+++ 2633-convert-object-to-json-string/2633-convert-object-to-json-string.ts
@@ -0,0 +1,29 @@
+function jsonStringify(object: any): string {
+    const checkTypes = (val: any): string => {
+        if (typeof val !== `object`)
+            if (typeof val === `string`) 
+                return `"${val}"`;
+            else return `${val}`;
+        else if (val === null)
+            return `null`;
+        else if (Array.isArray(val))
+            return `${stringifyArr(val)}`;
+        else return `${jsonStringify(val)}`;
+    }
+
+    const stringifyArr = (arr: any[]): string => arr.length > 0 ? arr.reduce((ans, e) => ans += `${checkTypes(e)},`, `[`).slice(0, -1) + `]` : `[]`;
+
+    if (typeof object !== `object`)
+        return checkTypes(object);
+    
+    if (object === null)
+        return `null`;
+
+    if (Array.isArray(object))
+        return stringifyArr(object);
+
+    if (Object.values(object).length === 0)
+        return `{}`;
+
+    return Object.keys(object).reduce((ans, key) => ans += `"${key}":${checkTypes(object[key])},`, `{`).slice(0, -1) + `}`;
+}
