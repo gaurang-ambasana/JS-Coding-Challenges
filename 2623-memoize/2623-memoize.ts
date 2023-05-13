a028efe9dfddf368ba4656074d4372ec46665a9f
Gaurang Ambasana <gpambasana@gmail.com>
2023-05-14 00:00:08 +0530

    Time: 367 ms (57.19%), Space: 111.6 MB (5.63%) - LeetHub

---
+++ 2623-memoize/2623-memoize.ts
@@ -0,0 +1,18 @@
+type Fn = (...params: any) => any
+
+function memoize(fn: Fn): Fn {
+    const cache = new Map<string, any>();
+
+    return (...args) => {
+        const key = args.join();
+
+        if (cache.has(key)) {
+            return cache.get(key);
+        }
+
+        const ans = fn(...args);
+        cache.set(key, ans);
+
+        return ans;
+    }
+}
