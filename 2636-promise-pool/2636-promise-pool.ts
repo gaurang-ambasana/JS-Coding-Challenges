e7754edb7e2ad21d4ee2778312ce253bf36c07e1
Gaurang Ambasana <gpambasana@gmail.com>
2023-05-17 23:09:00 +0530

    Time: 83 ms (7.04%), Space: 42.7 MB (83.45%) - LeetHub

---
+++ 2636-promise-pool/2636-promise-pool.ts
@@ -0,0 +1,7 @@
+type F = () => Promise<any>;
+
+function promisePool(functions: F[], n: number): Promise<any> {
+    const helper = async (): Promise<void> => n < functions.length && functions[n++]().then(helper);
+
+    return Promise.all(functions.slice(0, n).map(fn => fn().then(helper)));
+}
