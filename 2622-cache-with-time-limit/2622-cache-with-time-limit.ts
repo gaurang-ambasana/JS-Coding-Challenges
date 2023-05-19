f7c8178dc7f77ee2d78dcb477a2407da08b79cf8
Gaurang Ambasana <gpambasana@gmail.com>
2023-05-20 00:19:01 +0530

    Time: 70 ms (20.00%), Space: 43.4 MB (11.57%) - LeetHub

---
+++ 2622-cache-with-time-limit/2622-cache-with-time-limit.ts
@@ -0,0 +1,39 @@
+class TimeLimitedCache {
+    cache: Map<number, { value: number, timer: ReturnType<typeof setTimeout> }>; 
+    
+    constructor() {
+        this.cache = new Map();
+    }
+
+    set(key: number, value: number, duration: number): boolean {
+        if (this.cache.has(key)) {
+            clearTimeout(this.cache.get(key).timer);
+            
+            this.cache.set(key, {
+                value, 
+                timer: setTimeout(() => {
+                    this.cache.delete(key);
+                }, duration),
+            });
+
+            return true;
+        }
+
+        this.cache.set(key, {
+            value, 
+            timer: setTimeout(() => {
+                this.cache.delete(key);
+            }, duration),
+        });
+
+        return false;
+    }
+
+    get(key: number): number {
+        return this.cache.get(key)?.value ?? -1;
+    }
+
+	count(): number {
+        return this.cache.size;
+    }
+}
