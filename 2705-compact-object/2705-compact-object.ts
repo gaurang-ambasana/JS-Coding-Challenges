75aac633e2418d7f15dfdb4c00bb442940bde0db
Gaurang Ambasana <gpambasana@gmail.com>
2023-06-22 22:48:59 +0530

    Time: 103 ms (64.57%), Space: 54.6 MB (47.53%) - LeetHub

--- 2705-compact-object/2705-compact-object.ts
+++ 2705-compact-object/2705-compact-object.ts
@@ -3,8 +3,7 @@ type Obj = Record<any, any>;
 function compactObject(obj: Obj): Obj {
     if (Array.isArray(obj))
         obj = obj.filter(Boolean);
-    else
-        for (const prop in obj)
+    else for (const prop in obj)
             if (!obj[prop]) delete obj[prop];
 
     for (const prop in obj)
