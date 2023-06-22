b5df784ba716c46225a6763838f72c6a0d24b5e0
Gaurang Ambasana <gpambasana@gmail.com>
2023-06-22 12:24:58 +0530

    Time: 57 ms (81.01%), Space: 43.2 MB (36.29%) - LeetHub

---
+++ 2726-calculator-with-method-chaining/2726-calculator-with-method-chaining.ts
@@ -0,0 +1,37 @@
+class Calculator {
+  init: number;
+  
+  constructor(value : number) {
+    this.init = value;
+  }
+    
+  add(value : number) : Calculator {
+    this.init += value;
+    return this;
+  }
+    
+  subtract(value : number) : Calculator {
+    this.init -= value;
+    return this;
+  }
+    
+  multiply(value : number) : Calculator {
+    this.init *= value;
+    return this;
+  }
+
+  divide(value : number) : Calculator {
+    if (value === 0) throw Error("Division by zero is not allowed");
+    this.init /= value;
+    return this;
+  }
+    
+  power(value : number) : Calculator {
+    this.init = this.init ** value;
+    return this;
+  }
+
+  getResult() : number {
+    return this.init;
+  }
+}
