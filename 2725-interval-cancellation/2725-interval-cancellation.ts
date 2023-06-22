5f4e1de61573a495957406293babafad7563ccd1
Gaurang Ambasana <gpambasana@gmail.com>
2023-06-22 12:29:54 +0530

    Time: 78 ms (48.98%), Space: 42.7 MB (90.61%) - LeetHub

---
+++ 2725-interval-cancellation/2725-interval-cancellation.ts
@@ -0,0 +1,8 @@
+function cancellable(fn: Function, args: any[], t: number): Function {
+    fn(...args);
+    const timer = setInterval(() => {
+        fn(...args);
+    }, t);
+
+    return () => clearTimeout(timer);
+}
